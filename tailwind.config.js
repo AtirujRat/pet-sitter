11b7a02063677ce799802282c1392bd32cee03c1
AtirujRat <136755600+RattRuj@users.noreply.github.com>
2024-07-05 15:44:29 +0700

    initilize project

---
+++ tailwind.config.js
@@ -0,0 +1,110 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+
+    fontSize: {
+      h1: [
+        "3.5rem",
+        {
+          lineHeight: "4rem",
+          fontWeight: "bold",
+        },
+      ],
+      h2: [
+        "2.25rem",
+        {
+          lineHeight: "2.75rem",
+          fontWeight: "bold",
+        },
+      ],
+      h3: [
+        "1.5rem",
+        {
+          lineHeight: "2rem",
+          fontWeight: "bold",
+        },
+      ],
+      h4: [
+        "1.25rem",
+        {
+          lineHeight: "1.75rem",
+          fontWeight: "bold",
+        },
+      ],
+      b1: [
+        "1.125rem",
+        {
+          lineHeight: "1.625rem",
+          fontWeight: 500,
+        },
+      ],
+      b2: [
+        "1rem",
+        {
+          lineHeight: "1.75rem",
+          fontWeight: 500,
+        },
+      ],
+      b3: [
+        "0.875rem",
+        {
+          lineHeight: "1.5rem",
+          fontWeight: 500,
+        },
+      ],
+    },
+
+    colors: {
+      ps: {
+        gray: {
+          100: "#FFFFFF",
+          200: "#F6F6F9",
+          300: "#AEB1C3",
+          400: "#7B7E8F",
+          500: "#5B5D6F",
+          600: "#3A3B46",
+        },
+        orange: {
+          100: "#FFF1EC",
+          200: "#FFD5C2",
+          300: "#FFB899",
+          400: "#FF986F",
+          500: "#FF7037",
+          600: "#E44A0C",
+        },
+        yellow: {
+          100: "#FFF5EC",
+          200: "#FFCA62",
+        },
+        blue: {
+          100: "#ECFBFF",
+          500: "#76D0FC",
+        },
+        green: {
+          100: "#E7FDF4",
+          500: "#1CCD83",
+        },
+        pink: {
+          100: "#FFF0F1",
+          500: "#FA8AC0",
+        },
+        black: "#000000",
+        white: "#FFFFFF",
+        red: "#EA1010",
+      },
+    },
+    plugins: [require("daisyui")],
+  },
+};
