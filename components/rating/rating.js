ffd61227fb931b3575a4278a9274b15159216cc3
wiKanok <kanokwan1401.ks@gmail.com>
2024-07-09 05:25:03 +0700

    feat: add validation for input select value

--- components/rating/rating.js
+++ components/rating/rating.js
@@ -4,10 +4,17 @@ export default function RatingStars() {
     <>
       <div className="flex gap-2">
         {rate.reverse().map((x, index) => (
-          <button className="flex px-2 py-1 items-center justify-center gap-1 border border-[#DCDFED] rounded-lg">
+          <button
+            key={index}
+            className="flex px-2 py-1 items-center justify-center gap-1 border border-[#DCDFED] rounded-lg"
+          >
             <p className="text-b2 text-ps-gray-400">{x}</p>
             {Array.from({ length: x }).map((rate, index2) => (
-              <img src="/assets/star-rating.svg" alt="Star Rating" />
+              <img
+                src="/assets/star-rating.svg"
+                alt="Star Rating"
+                key={index2}
+              />
             ))}
           </button>
         ))}
