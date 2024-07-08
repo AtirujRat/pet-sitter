1ef0782240eaf7247449267b6a52965d6342e8ab
wiKanok <kanokwan1401.ks@gmail.com>
2024-07-08 15:19:18 +0700

    feat:Add logout functionality to the navigation bar

--- components/checkbox/checkbox.js
+++ components/checkbox/checkbox.js
@@ -1,14 +1,14 @@
 export default function Checkbox() {
   return (
     <div className="bg-ps-gray-200 px-6 gap-[26px] w-full h-[72px] flex items-center space-x-4 text-ps-gray-600 rounded-t-3xl">
-      <p className="text-ps-gray-600 font-bold">Pet Type:</p>
+      <div className="text-ps-gray-600 font-bold bg-[#F6F6F9]">Pet Type:</div>
       <span className="form-control ">
         <label className="cursor-pointer label flex items-center space-x-4 bg-ps-gray-200">
           <input
             type="checkbox"
-            className="checkbox checkbox-warning border border-ps-gray-300"
+            className="checkbox checkbox-warning border border-ps-gray-300 "
           />
-          <span className="label-text text-[16px] font-medium">Dog</span>
+          <span className="label-text text-[16px] font-medium ">Dog</span>
           <input
             type="checkbox"
             className="checkbox checkbox-warning border border-ps-gray-300"
