631c032de56f4299bc4f7e707e55bf99fc69c102
PNKim <paljapon.nait@gmail.com>
2024-07-08 10:02:18 +0700

    fix: rebase conflict

--- pages/components/layout.js
+++ pages/components/layout.js
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 
 export default function Layout({ children }) {
   const router = useRouter();
-  const noLayoutRoutes = ["/login", "/register"];
+  const noLayoutRoutes = ["/login", "/register", "/register/owner"];
 
   return (
     <>
--- pages/register/owner/index.js
+++ pages/register/owner/index.js
@@ -1,16 +1,13 @@
-import BackgroudAuth from "../components/authentication/BackgroudAuth";
-import RegisterForm from "../components/authentication/RegisterForm";
-import SocialLogin from "../components/authentication/SocialLogin";
+import BackgroudAuth from "../../components/authentication/BackgroudAuth";
+import RegisterForm from "../../components/authentication/RegisterForm";
+import SocialLogin from "../../components/authentication/SocialLogin";
 
 export default function RegisterOwner() {
   return (
     <section className="w-full h-screen flex justify-center items-center">
       <BackgroudAuth />
-      <div
-        id="form"
-        className="w-[30%] flex flex-col items-center justify-center gap-14"
-      >
-        <div id="head-form" className="text-center">
+      <div className="w-[30%] flex flex-col items-center justify-center gap-14">
+        <div className="text-center text-shadow">
           <h1 className="text-h1 text-ps-black">Join Us!</h1>
           <h3 className="text-h3 text-ps-gray-400">
             Find your perfect pet sitter with us
