6397697283b061a3eba18680028ad7f5429b1c52
wiKanok <kanokwan1401.ks@gmail.com>
2024-07-08 13:33:25 +0700

    feat:create rating button in home page

---
+++ components/rating/rating.js
@@ -0,0 +1,17 @@
+export default function RatingStars() {
+  const rate = [1, 2, 3, 4, 5];
+  return (
+    <>
+      <div className="flex gap-2">
+        {rate.reverse().map((x, index) => (
+          <button className="flex px-2 py-1 items-center justify-center gap-1 border border-[#DCDFED] rounded-lg">
+            <p className="text-b2 text-ps-gray-400">{x}</p>
+            {Array.from({ length: x }).map((rate, index2) => (
+              <img src="/assets/star-rating.svg" alt="Star Rating" />
+            ))}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+}
