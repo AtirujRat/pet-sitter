ec0f4c1c7566ca16c97d099e5348b77166d0be9d
wiKanok <kanokwan1401.ks@gmail.com>
2024-07-08 10:44:14 +0700

    feat:create checkbox in home page

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <section className="w-full flex justify-center bg-ps-black md:h-[280px] text-ps-white">
-      <div className="flex-col items-center text-center max-w-[1440px] min-w-0 w-full px-20 py-20">
+      <div className="flex-col items-center text-center max-w-[1440px] min-w-0 w-full px-20 py-20 bg-ps-black">
         <div className="flex justify-center items-center ">
           <img
             src="/assets/sister-logo-white.svg"
