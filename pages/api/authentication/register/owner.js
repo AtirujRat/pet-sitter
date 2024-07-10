10777acfecc9284ae63a1a799122c4eba21cfb8d
PNKim <paljapon.nait@gmail.com>
2024-07-10 10:06:22 +0700

    feat: can show-hide password

--- pages/api/authentication/register/owner.js
+++ pages/api/authentication/register/owner.js
@@ -7,15 +7,15 @@ export default async function handler(req, res) {
     return res.status(404).json({ message: "missing data from request" });
   }
 
-  // let { data, error } = await supabase.auth.signUp({
-  //   email: newOwner.email,
-  //   password: newOwner.password,
-  //   phone: newOwner.phone,
-  // });
-  // console.log(error);
-  // if (error) {
-  //   return res.status(400).json({ name: "error connection from database" });
-  // }
+  let { data, error } = await supabase.auth.signUp({
+    email: newOwner.email,
+    password: newOwner.password,
+    phone: newOwner.phone,
+  });
+  console.log(error);
+  if (error) {
+    return res.status(400).json({ name: "error connection from database" });
+  }
 
   const salt = await bcrypt.genSalt(10);
   newOwner.password = await bcrypt.hash(newOwner.password, salt);
