6a6d58d5b5b4be4e95ccd3c39f28519bc572a10f
PNKim <paljapon.nait@gmail.com>
2024-07-11 14:34:30 +0700

    feat: add static map

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import HeaderHomes from "@/components/home/HeaderHome";
 import ContentHomePage from "@/components/home/ContentHomePage";
 import FindAPetSister from "@/components/home/FindAPetSister";
+import Map from "@/components/Map";
 
 export default function Home() {
   return (
+    // <Map />
     <section className="w-full flex-col items-center lg:pt-20 pt-10 overflow-hidden ">
       <HeaderHomes />
       <ContentHomePage />
