fe2992026f3479d74ba43c59b982db780c628b4a
Ja <ja.techup@gmail.com>
2024-07-11 18:18:20 +0700

    feat: create get specific sitter api and fetch data on details page

---
+++ pages/api/sitters/[id].js
@@ -0,0 +1,27 @@
+import { supabase } from "@/utils/supabase";
+
+export default async function handler(req, res) {
+  const { id } = req.query;
+  if (req.method === "GET") {
+    try {
+      let { data: sitters, error } = await supabase
+        .from("sitters")
+        .select(
+          "id, full_name, trade_name, experience, introduction, services, place_description, profile_image_url, sitters_images(image_url), pet_types(id,pet_type), bookings(reviews!inner(rating))"
+        )
+        .eq("id", id);
+
+      if (error) {
+        throw error;
+      }
+
+      return res.status(200).json({
+        data: sitters,
+      });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  } else {
+    return res.status(405).json({ error: "Method Not Available" });
+  }
+}
