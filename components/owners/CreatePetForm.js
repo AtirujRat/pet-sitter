6313fc6bacbd3b7cd51542e1546ed7ad1d839ddd
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-07-11 20:50:19 +0700

    feat: add validation create pet form

--- components/owners/CreatePetForm.js
+++ components/owners/CreatePetForm.js
@@ -1,136 +1,249 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import Image from "next/image";
 
+const onSubmit = (values, actions) => {
+  console.log(values);
+  actions.setSubmitting(false);
+};
+
+function validateRequired(value) {
+  let error;
+  if (!value === undefined || value === null || value === "") {
+    error = " Required";
+  }
+  return error;
+}
+
 export default function CreatePetForm() {
+  const initialValues = {
+    petName: "",
+    petType: "",
+    breed: "",
+    sex: "",
+    age: "",
+    color: "",
+    weight: "",
+    about: "",
+  };
+
   return (
-    <section className="w-[75%] h-fit shadow-lg rounded-xl bg-ps-white">
-      <div className="flex flex-col justify-center p-10 gap-10">
-        <p className="flex text-h3 gap-2">
-          <Image
-            src="/assets/pets/Vector 257.svg"
-            alt=""
-            width={10}
-            height={10}
-            className="rounded-t-3xl rounded-full"
-          />
-          Your Pet
-        </p>
-        {/* add image pet */}
-        <Image
-          src="/assets/pets/pet-dummy.svg"
-          alt=""
-          width={240}
-          height={240}
-          className=""
-        />
-        {/* pet name */}
-        <div className="max-sm:hidden flex flex-col">
-          <label htmlFor="name" className="text-[16px] font-bold pb-1">
-            Pet Name*
-          </label>
-          <input
-            type="text"
-            className="border-[#DCDFED] text-[#7B7E8F] rounded-lg"
-            placeholder="John Wick"
-          />
-        </div>
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ isSubmitting, errors, touched }) => (
+        <Form className="w-[75%] h-fit shadow-lg rounded-xl bg-ps-white p-10">
+          <div className="flex flex-col gap-10">
+            <p className="flex text-h3 gap-2">Your Pet</p>
+            {/* Image */}
+            <div className="cursor-pointer w-fit h-fit">
+              <Image
+                src="/assets/pets/pet-dummy.svg"
+                alt="Dummy Pet Image"
+                width={240}
+                height={240}
+              />
+            </div>
+            {/* Pet Name */}
+            <div className="max-sm:hidden flex flex-col">
+              <label
+                htmlFor="petName"
+                className="text-[16px] font-bold pb-1 flex"
+              >
+                Pet Name*
+                <ErrorMessage
+                  name="petName"
+                  component="div"
+                  className="text-ps-red text-b3"
+                />
+              </label>
+              <Field
+                type="text"
+                id="petName"
+                name="petName"
+                validate={validateRequired}
+                className="border-[#DCDFED] text-[#7B7E8F] rounded-lg"
+                placeholder="John Wick"
+              />
+            </div>
 
-        <div className="flex justify-between">
-          <div className="flex flex-col w-[48%]">
-            <p className="text-[16px] font-bold">Pet Type*</p>
-            <select
-              className="select select-bordered w-full outline-none ring-0 border-[#DCDFED] text-[#7B7E8F] font-normal text-[16px]"
-              placeholder="Select your pet type"
-            >
-              <option value="Dog">Dog</option>
-              <option value="Cat">Cat</option>
-              <option value="Bird">Bird</option>
-              <option value="Bird">Bird</option>
-              <option value="Rabbit">Rabbit</option>
-            </select>
-          </div>
-          {/* Breed of pet */}
-          <div className="flex flex-col w-[48%]">
-            <label htmlFor="breed" className="text-[16px] font-bold pb-1">
-              Breed*
-            </label>
-            <input
-              type="text"
-              className="border-[#DCDFED] text-[#7B7E8F] rounded-lg"
-              placeholder="Breed of your pet"
-            />
-          </div>
-        </div>
+            <div className="flex justify-between">
+              {/* Pet Type */}
+              <div className="flex flex-col w-[48%]">
+                <label htmlFor="petType" className="flex text-[16px] font-bold">
+                  Pet Type*
+                  <ErrorMessage
+                    name="petType"
+                    component="div"
+                    className="text-ps-red text-b3"
+                  />
+                </label>
 
-        <div className="flex justify-between">
-          <div className="flex flex-col w-[48%]">
-            {/* sex of pet */}
-            <p className="text-[16px] font-bold">Sex*</p>
-            <select
-              className="select select-bordered w-full outline-none ring-0 border-[#DCDFED] text-[#7B7E8F] font-normal text-[16px]"
-              placeholder="Select sex of your pet"
-            >
-              <option value="Dog">Male</option>
-              <option value="Cat">Female</option>
-            </select>
-          </div>
-          {/* age of pet */}
-          <div className="flex flex-col w-[48%]">
-            <label htmlFor="age" className="text-[16px] font-bold pb-1">
-              Age (Mouth)*
-            </label>
-            <input
-              type="text"
-              className="border-[#DCDFED] text-[#7B7E8F] rounded-lg"
-              placeholder="Age of your pet"
-            />
-          </div>
-        </div>
+                <Field
+                  as="select"
+                  id="petType"
+                  name="petType"
+                  validate={validateRequired}
+                  className="select select-bordered w-full outline-none ring-0 border-[#DCDFED] text-[#7B7E8F] font-normal text-[16px]"
+                >
+                  <option value="">Select your pet type</option>
+                  <option value="Dog">Dog</option>
+                  <option value="Cat">Cat</option>
+                  <option value="Bird">Bird</option>
+                  <option value="Rabbit">Rabbit</option>
+                </Field>
+              </div>
+              {/* Breed */}
+              <div className="flex flex-col w-[48%]">
+                <label
+                  htmlFor="breed"
+                  className="flex text-[16px] font-bold pb-1"
+                >
+                  Breed*
+                  <ErrorMessage
+                    name="breed"
+                    component="div"
+                    className="text-ps-red text-b3"
+                  />
+                </label>
+                <Field
+                  type="text"
+                  id="breed"
+                  name="breed"
+                  validate={validateRequired}
+                  className="border-[#DCDFED] text-[#7B7E8F] rounded-lg"
+                  placeholder="Breed of your pet"
+                />
+              </div>
+            </div>
 
-        <div className="flex justify-between">
-          {/* color of pet */}
-          <div className="flex flex-col w-[48%]">
-            <label htmlFor="color" className="text-[16px] font-bold pb-1">
-              Color*
-            </label>
-            <input
-              type="text"
-              className="border-[#DCDFED] text-[#7B7E8F] rounded-lg"
-              placeholder="Describe color of your pet"
-            />
-          </div>
-          <div className="flex flex-col w-[48%]">
-            <label htmlFor="color" className="text-[16px] font-bold pb-1">
-              Weight (Kilogram)*
-            </label>
-            <input
-              type="text"
-              className="border-[#DCDFED] text-[#7B7E8F] rounded-lg"
-              placeholder="Weight of your per"
-            />
-          </div>
-        </div>
-        {/* about of pet */}
-        <div className="flex flex-col w-full">
-          <label htmlFor="color" className="text-[16px] font-bold pb-1">
-            About
-          </label>
-          <textarea
-            type="text"
-            className="border-[#DCDFED] text-[#7B7E8F] rounded-lg h-[140px]"
-            placeholder="Describe more about your pet..."
-          />
-        </div>
+            <div className="flex justify-between">
+              {/* Sex */}
+              <div className="flex flex-col w-[48%]">
+                <label htmlFor="sex" className="flex text-[16px] font-bold">
+                  Sex*
+                  <ErrorMessage
+                    name="sex"
+                    component="div"
+                    className="text-ps-red text-b3"
+                  />
+                </label>
+                <Field
+                  as="select"
+                  id="sex"
+                  name="sex"
+                  validate={validateRequired}
+                  className="select select-bordered w-full outline-none ring-0 border-[#DCDFED] text-[#7B7E8F] font-normal text-[16px]"
+                >
+                  <option value="">Select sex of your pet</option>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                </Field>
+              </div>
+              {/* Age */}
+              <div className="flex flex-col w-[48%]">
+                <label
+                  htmlFor="age"
+                  className="flex text-[16px] font-bold pb-1"
+                >
+                  Age (Months)*
+                  <ErrorMessage
+                    name="age"
+                    component="div"
+                    className="text-ps-red text-b3"
+                  />
+                </label>
+                <Field
+                  type="number"
+                  id="age"
+                  name="age"
+                  validate={validateRequired}
+                  className="border-[#DCDFED] text-[#7B7E8F] rounded-lg"
+                  placeholder="Age of your pet"
+                  min="1"
+                />
+              </div>
+            </div>
 
-        {/* button form */}
-        <div className="flex flex-wrap gap-4 justify-between">
-          <button className="w-[127px] bg-ps-orange-100 text-ps-orange-500 text-[16px] font-bold rounded-full tracking-wide h-[48px]">
-            Cancel
-          </button>
-          <button className="w-[127px] bg-ps-orange-500 text-ps-white text-[16px] font-bold rounded-full tracking-wide h-[48px]">
-            Create Pet
-          </button>
-        </div>
-      </div>
-    </section>
+            <div className="flex justify-between">
+              {/* Color */}
+              <div className="flex flex-col w-[48%]">
+                <label
+                  htmlFor="color"
+                  className="flex text-[16px] font-bold pb-1"
+                >
+                  Color*
+                  <ErrorMessage
+                    name="color"
+                    component="div"
+                    className="text-ps-red text-b3"
+                  />
+                </label>
+                <Field
+                  type="text"
+                  id="color"
+                  name="color"
+                  validate={validateRequired}
+                  className="border-[#DCDFED] text-[#7B7E8F] rounded-lg"
+                  placeholder="Describe color of your pet"
+                />
+              </div>
+              {/* Weight */}
+              <div className="flex flex-col w-[48%]">
+                <label
+                  htmlFor="weight"
+                  className="flex text-[16px] font-bold pb-1"
+                >
+                  Weight (Kilogram)*
+                  <ErrorMessage
+                    name="weight"
+                    component="div"
+                    className="text-ps-red text-b3"
+                  />
+                </label>
+                <Field
+                  type="number"
+                  id="weight"
+                  name="weight"
+                  validate={validateRequired}
+                  className="border-[#DCDFED] text-[#7B7E8F] rounded-lg"
+                  placeholder="Weight of your pet"
+                  min="1"
+                  step="0.01"
+                />
+              </div>
+            </div>
+
+            {/* About */}
+            <div className="flex flex-col w-full">
+              <label htmlFor="about" className="text-[16px] font-bold pb-1">
+                About
+              </label>
+              <Field
+                as="textarea"
+                id="about"
+                name="about"
+                className="border-[#DCDFED] text-[#7B7E8F] rounded-lg h-[140px]"
+                placeholder="Describe more about your pet..."
+              />
+            </div>
+
+            {/* Buttons */}
+            <div className="flex flex-wrap gap-4 justify-between">
+              <button
+                type="button"
+                className="w-[127px] bg-ps-orange-100 text-ps-orange-500 text-[16px] font-bold rounded-full tracking-wide h-[48px]"
+              >
+                Cancel
+              </button>
+              <button
+                type="submit"
+                className="w-[127px] bg-ps-orange-500 text-ps-white text-[16px] font-bold rounded-full tracking-wide h-[48px]"
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? "Creating..." : "Create Pet"}
+              </button>
+            </div>
+          </div>
+        </Form>
+      )}
+    </Formik>
   );
 }
