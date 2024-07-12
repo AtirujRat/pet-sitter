efaa2e91e8f408fc5ffcddbf3189f280836059da
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-07-12 14:56:27 +0700

    feat: edit update pet form

--- components/owners/yourpet/PetCard.js
+++ components/owners/yourpet/PetCard.js
@@ -7,13 +7,21 @@ export default function PetCard({ name, type }) {
       <div className="container-card flex flex-col justify-center items-center gap-3">
         <Image
           src="/assets/pets/pet-dummy.svg"
+<<<<<<< HEAD
           alt="image"
+=======
+          alt={`${type.toLowerCase()}`}
+>>>>>>> 5c2ccd2 (feat: edit update pet form)
           width={100}
           height={100}
           className="rounded-t-3xl rounded-full"
         />
         <p className="text-b1 text-center">{name}</p>
+<<<<<<< HEAD
         <PetTag type={type} />
+=======
+        <PetTag type={type.toLowerCase()} />
+>>>>>>> 5c2ccd2 (feat: edit update pet form)
       </div>
     </section>
   );
