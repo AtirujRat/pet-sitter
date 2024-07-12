758c45c2f83a6134b3a5ecfae11a1ba177d7cc3f
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-07-12 14:34:39 +0700

    feat: create update pet page

---
+++ pages/owners/petlist/update/index.js
@@ -0,0 +1,15 @@
+import CreatePetForm from "@/components/owners/CreatePetForm";
+
+export default function PetList() {
+  return (
+    <section className="w-full flex justify-center bg-ps-gray-100 sm:py-8 pt-4 lg:pb-32">
+      <div className="page-container w-full sm:px-20 max-w-[1440px]">
+        <div className="flex gap-8 max-lg:flex-col">
+          {/* mock up menu account */}
+          <div className="w-[284px] h-[356px] bg-ps-white sm:rounded-2xl lg:shadow-lg sm:p-6 lg:sticky top-5"></div>
+          <CreatePetForm />
+        </div>
+      </div>
+    </section>
+  );
+}
