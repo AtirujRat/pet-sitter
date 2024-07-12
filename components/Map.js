54fcb2a5d87ee9c1520cfeca5602ff16ad49c418
PNKim <paljapon.nait@gmail.com>
2024-07-12 09:11:39 +0700

    feat: test map with address

--- components/Map.js
+++ components/Map.js
@@ -1,47 +1,68 @@
-"use client";
 import React from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
-import { useState, useCallback } from "react";
+import { GoogleMap, useJsApiLoader, MarkerF } from "@react-google-maps/api";
+import { useSearch } from "@/pages/context/Search";
+// import { useState } from "react";
 
 export default function Map() {
+  const { searchLat, searchLng } = useSearch();
   const containerStyle = {
     width: "1000px",
     height: "800px",
   };
 
   const center = {
-    lat: 51.51035,
-    lng: 0.1022,
+    lat: searchLat,
+    lng: searchLng,
+  };
+
+  const options = {
+    mapId: "d5dd20a527464be2",
+    mapTypeControl: false, // map or satellite
+    zoomControl: false, // zoom button + -
+    // fullscreenControl: false, fullscreen button
+    clickableIcons: false,
+    scrollwheel: true,
+    streetViewControl: false,
   };
 
   const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
+    id: options.mapId,
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   });
 
-  const [map, setMap] = useState(null);
-
-  const onLoad = (map) => {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-
-    // setMap(map);
+  const pinIcon = {
+    url: "/assets/map/pin@2x.png",
+    scaledSize: { width: 50, height: 50 },
   };
 
-  const onUnmount = (map) => {
-    setMap(null);
+  const markerClicked = (event) => {
+    console.log(event.latLng.lat());
+    console.log(event.latLng.lng());
+  };
+
+  const markerFinishDrag = (event) => {
+    console.log(event.latLng.lat());
+    console.log(event.latLng.lng());
   };
 
   return isLoaded ? (
     <GoogleMap
       mapContainerStyle={containerStyle}
+      options={options}
       center={center}
-      zoom={12}
-      onLoad={map}
-      onUnmount={onUnmount}
+      zoom={15}
+      //   onLoad={map}
+      //   onUnmount={onUnmount}
     >
       {/* Child components, such as markers, info windows, etc. */}
+      <MarkerF
+        icon={pinIcon}
+        position={{ lat: searchLat, lng: searchLng }}
+        draggable
+        onClick={markerClicked}
+        onDragEnd={markerFinishDrag}
+        // visible={false}
+      />
       <></>
     </GoogleMap>
   ) : (
