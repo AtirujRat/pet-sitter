3517dc2e1f114cbb216596e4e8120ee231ac3264
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-07-12 14:56:27 +0700

    feat: edit update pet form

--- components/owners/yourpet/PetList.js
+++ components/owners/yourpet/PetList.js
@@ -1,76 +1,8 @@
 import PetCard from "./PetCard";
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Link from "next/link";
 import { useRouter } from "next/router";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { useEffect, useState } from "react";
-import axios from "axios";
-
-const API_URL = "/api/owners";
-
-export default function PetList() {
-  const router = useRouter();
-  const { id } = router.query;
-  const [pets, setPets] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchPets = async () => {
-      setLoading(true);
-      setError(null);
-
-      try {
-        if (id) {
-          const response = await axios.get(`${API_URL}/${id}`);
-
-          if (response.data && Array.isArray(response.data)) {
-            setPets(response.data);
-          } else {
-            setError("Data received is not in expected format or empty");
-          }
-        }
-
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching pets:", error.message);
-        setError("Error fetching pets");
-        setLoading(false);
-      }
-    };
-
-    fetchPets();
-  }, [id]);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-=======
-=======
-import Link from "next/link";
->>>>>>> bf2cbb2 (feat: update create pet form)
-=======
-import Link from "next/link";
-=======
->>>>>>> d3a647f (feat: edit update pet form)
->>>>>>> 19e2e33 (feat: edit update pet form)
-=======
-import Link from "next/link";
->>>>>>> 1d054ed (feat: edit update pet form)
-=======
->>>>>>> 1919ac5 (feat: set path of pet list)
-=======
 import { useEffect, useState } from "react";
 import { supabase } from "@/utils/supabase";
->>>>>>> b87b3ee (feat: create api get pet)
 
 export default function YourPage() {
   const router = useRouter();
@@ -117,85 +49,26 @@ export default function YourPage() {
     return <div>Loading...</div>;
   }
 
-<<<<<<< HEAD
-  const pets = [
-    { name: "Bubba", type: "Dog" },
-    { name: "Daisy", type: "Dog" },
-    { name: "I Som", type: "Cat" },
-    { name: "Noodle Bird", type: "Bird" },
-  ];
->>>>>>> 5c2ccd2 (feat: edit update pet form)
-=======
   if (error) {
     return <div>Error: {error}</div>;
   }
->>>>>>> b87b3ee (feat: create api get pet)
 
   return (
     <section className="w-[75%] min-h-[824px] h-fit shadow-lg rounded-xl bg-ps-white">
       <div className="flex flex-col justify-center p-10 gap-10">
         <div className="flex justify-between items-center">
           <p className="text-h3">Your Pet</p>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
           <Link href={`/owners/${id}/yourpet/create`}>
-=======
-=======
->>>>>>> 19e2e33 (feat: edit update pet form)
-=======
->>>>>>> 1d054ed (feat: edit update pet form)
-          <Link href="/owners/yourpet/create">
->>>>>>> bf2cbb2 (feat: update create pet form)
-=======
-          <Link href={`/owners/${id}/yourpet/create`}>
->>>>>>> 1919ac5 (feat: set path of pet list)
             <button className="w-[127px] bg-ps-orange-500 text-ps-white text-[16px] font-bold rounded-full tracking-wide h-[48px]">
               Create Pet
             </button>
           </Link>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         </div>
         <div className="flex flex-wrap gap-4 justify-stretch">
-          {pets.map((pet) => (
-            <Link key={pet.id} href={`/owners/${id}/yourpet/${pet.id}`}>
-              <PetCard key={pet.id} name={pet.name} type={pet.type} />
-            </Link>
-=======
->>>>>>> 19e2e33 (feat: edit update pet form)
-=======
-          <button className="w-[127px] bg-ps-orange-500 text-ps-white text-[16px] font-bold rounded-full tracking-wide h-[48px]">
-            Create Pet
-          </button>
-<<<<<<< HEAD
-=======
->>>>>>> bf2cbb2 (feat: update create pet form)
-=======
->>>>>>> d3a647f (feat: edit update pet form)
->>>>>>> 19e2e33 (feat: edit update pet form)
-=======
->>>>>>> 1d054ed (feat: edit update pet form)
-        </div>
-        <div className="flex flex-wrap gap-4 justify-stretch">
-<<<<<<< HEAD
-          {pets.map((pet, index) => (
-<<<<<<< HEAD
-            <PetCard key={index} name={pet.name} type={pet.type} />
->>>>>>> 5c2ccd2 (feat: edit update pet form)
-=======
-            <Link key={pet.id} href={`/owners/${id}/yourpet/${pet.id}/update`}>
-              <PetCard key={index} name={pet.name} type={pet.type} />
-=======
           {pets.map((pet) => (
             <Link key={pet.id} href={`/owners/${id}/yourpet/${pet.id}`}>
               <PetCard key={pet.id} name={pet.name} type={pet.pet_type} />
->>>>>>> b87b3ee (feat: create api get pet)
             </Link>
->>>>>>> 1919ac5 (feat: set path of pet list)
           ))}
         </div>
       </div>
--- components/owners/yourpet/UpdatePetForm.js
+++ components/owners/yourpet/UpdatePetForm.js
@@ -1,107 +1,28 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import Image from "next/image";
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { useEffect, useState } from "react";
-=======
->>>>>>> 8ff4edd (refactor: edit update pet form)
-import { useRouter } from "next/router";
-import axios from "axios";
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import { useEffect, useState } from "react";
-
-<<<<<<< HEAD
-const API_URL = "/api/pets";
->>>>>>> 8ff4edd (refactor: edit update pet form)
-=======
-const API_URL = "/api/owners";
->>>>>>> 9dc1fd7 (refactor: update api pets)
-
-export default function UpdatePetForm() {
-  const router = useRouter();
-  const { id, petId } = router.query;
-
-<<<<<<< HEAD
-  const [initialValues, setInitialValues] = useState({
-    name: "",
-    pet_type: "",
-=======
-=======
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { supabase } from "@/utils/supabase";
->>>>>>> b87b3ee (feat: create api get pet)
-
-const onSubmit = async (values, actions) => {
-  try {
-    // Perform the update operation here
-    const { data, error } = await supabase
-      .from("pets")
-      .update(values)
-      .eq("id", values.petId)
-      .eq("owner_id", values.ownerId);
-
-    if (error) {
-      console.error("Error updating pet data:", error);
-    } else {
-      console.log("Pet data updated successfully:", data);
-    }
-  } catch (error) {
-    console.error("An unexpected error occurred:", error);
-  }
-  actions.setSubmitting(false);
-};
-
-function validateRequired(value) {
-  let error;
-  if (value === undefined || value === null || value === "") {
-    error = " Required";
-  }
-  return error;
-}
-=======
->>>>>>> 6fecf16 (feat: create api update pet)
+import axios from "axios";
 
 export default function UpdatePetForm() {
-<<<<<<< HEAD
-  const initialValues = {
-    petName: "",
-    petType: "",
->>>>>>> 5c2ccd2 (feat: edit update pet form)
-=======
   const router = useRouter();
   const { Id, petId } = router.query;
 
   const [initialValues, setInitialValues] = useState({
     name: "",
     pet_type: "",
->>>>>>> b87b3ee (feat: create api get pet)
     breed: "",
     sex: "",
     age: "",
     color: "",
     weight: "",
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b87b3ee (feat: create api get pet)
     description: "",
   });
 
   useEffect(() => {
     const fetchPetData = async () => {
-<<<<<<< HEAD
-<<<<<<< HEAD
       if (Id && petId) {
-=======
-      if (ownerId && petId) {
->>>>>>> b87b3ee (feat: create api get pet)
-=======
-      if (Id && petId) {
->>>>>>> 6fecf16 (feat: create api update pet)
         const { data, error } = await supabase
           .from("pets")
           .select(
@@ -121,15 +42,7 @@ export default function UpdatePetForm() {
           .single();
 
         if (error) {
-<<<<<<< HEAD
-<<<<<<< HEAD
           console.error("Error fetching pet data:", error.message);
-=======
-          console.error("Error fetching pet data:", error);
->>>>>>> b87b3ee (feat: create api get pet)
-=======
-          console.error("Error fetching pet data:", error.message);
->>>>>>> 6fecf16 (feat: create api update pet)
         } else {
           setInitialValues(data);
         }
@@ -137,75 +50,29 @@ export default function UpdatePetForm() {
     };
 
     fetchPetData();
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 6fecf16 (feat: create api update pet)
   }, [Id, petId]);
-=======
-  const [pet, setPet] = useState(null);
-
-  useEffect(() => {
-    const fetchPet = async () => {
-      try {
-        const response = await axios.get(`${API_URL}/${id}/${petId}`);
-        setPet(response.data);
-      } catch (error) {
-        console.error("Error fetching pet:", error);
-      }
-    };
-
-    if (id) {
-      fetchPet();
-    }
-  }, [petId]);
-
-  const initialValues = {
-    name: pet ? pet.name : "",
-    type: pet ? pet.type : "",
-    breed: pet ? pet.breed : "",
-    sex: pet ? pet.sex : "",
-    age: pet ? pet.age : "",
-    color: pet ? pet.color : "",
-    weight: pet ? pet.weight : "",
-    description: pet ? pet.description : "",
-  };
-
-  const validateRequired = (value) => {
-    let error;
-    if (!value || value === "") {
-      error = "Required";
-    }
-    return error;
-  };
->>>>>>> 8ff4edd (refactor: edit update pet form)
 
   const onSubmit = async (values, actions) => {
+    const { Id, petId } = router.query;
+
     try {
-      const response = await axios.put(`${API_URL}/${id}/${petId}`, values);
-      console.log("Response:", response.data);
-      router.push(`/owners/${id}/yourpet`);
+      const response = await axios.put(`/api/owners/${Id}/pets`, values);
+
+      console.log("Pet updated successfully:", response.data);
     } catch (error) {
-      console.error("Error updating pet:", error);
+      console.error("Error updating pet:", error.message);
     } finally {
       actions.setSubmitting(false);
     }
   };
 
-  const onDelete = async () => {
-    try {
-      const response = await axios.delete(`${API_URL}/${id}/${petId}`);
-      console.log("Pet deleted:", response.data);
-      router.push(`/owners/${id}/yourpet`);
-    } catch (error) {
-      console.error("Error deleting pet:", error);
+  function validateRequired(value) {
+    let error;
+    if (!value) {
+      error = "Required";
     }
-  };
-
-  if (!pet) {
-    return <p>Loading...</p>;
+    return error;
   }
-<<<<<<< HEAD
 
   return (
     <Formik
@@ -213,30 +80,7 @@ export default function UpdatePetForm() {
       onSubmit={onSubmit}
       enableReinitialize
     >
-<<<<<<< HEAD
-=======
-    about: "",
-  };
-
-  return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit}>
->>>>>>> 5c2ccd2 (feat: edit update pet form)
-=======
-  }, [ownerId, petId]);
-=======
->>>>>>> 6fecf16 (feat: create api update pet)
-
-  return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={onSubmit}
-      enableReinitialize
-    >
->>>>>>> b87b3ee (feat: create api get pet)
       {({ isSubmitting, errors, touched }) => (
-=======
-      {({ isSubmitting }) => (
->>>>>>> 8ff4edd (refactor: edit update pet form)
         <Form className="w-[75%] h-fit shadow-lg rounded-xl bg-ps-white p-10">
           <div className="flex flex-col gap-10">
             <p className="flex text-h3 gap-2">Your Pet</p>
@@ -251,45 +95,18 @@ export default function UpdatePetForm() {
             </div>
             {/* Pet Name */}
             <div className="max-sm:hidden flex flex-col">
-<<<<<<< HEAD
-<<<<<<< HEAD
               <label htmlFor="name" className="text-[16px] font-bold pb-1 flex">
                 Pet Name*
                 <ErrorMessage
                   name="name"
-=======
-              <label
-                htmlFor="petName"
-                className="text-[16px] font-bold pb-1 flex"
-              >
-                Pet Name*
-                <ErrorMessage
-                  name="petName"
->>>>>>> 5c2ccd2 (feat: edit update pet form)
-=======
-              <label htmlFor="name" className="text-[16px] font-bold pb-1 flex">
-                Pet Name*
-                <ErrorMessage
-                  name="name"
->>>>>>> b87b3ee (feat: create api get pet)
                   component="div"
                   className="text-ps-red text-b3"
                 />
               </label>
               <Field
                 type="text"
-<<<<<<< HEAD
-<<<<<<< HEAD
                 id="name"
                 name="name"
-=======
-                id="petName"
-                name="petName"
->>>>>>> 5c2ccd2 (feat: edit update pet form)
-=======
-                id="name"
-                name="name"
->>>>>>> b87b3ee (feat: create api get pet)
                 validate={validateRequired}
                 className="border-[#DCDFED] text-[#7B7E8F] rounded-lg"
                 placeholder="John Wick"
@@ -299,36 +116,13 @@ export default function UpdatePetForm() {
             <div className="flex justify-between">
               {/* Pet Type */}
               <div className="flex flex-col w-[48%]">
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b87b3ee (feat: create api get pet)
                 <label
                   htmlFor="pet_type"
                   className="flex text-[16px] font-bold"
                 >
-<<<<<<< HEAD
                   Pet Type*
                   <ErrorMessage
                     name="pet_type"
-=======
-                <label htmlFor="petType" className="flex text-[16px] font-bold">
-                  Pet Type*
-                  <ErrorMessage
-                    name="petType"
->>>>>>> 5c2ccd2 (feat: edit update pet form)
-=======
-                  Pet Type*
-                  <ErrorMessage
-                    name="pet_type"
->>>>>>> b87b3ee (feat: create api get pet)
-=======
-                <label htmlFor="type" className="flex text-[16px] font-bold">
-                  Pet Type*
-                  <ErrorMessage
-                    name="type"
->>>>>>> 8ff4edd (refactor: edit update pet form)
                     component="div"
                     className="text-ps-red text-b3"
                   />
@@ -336,44 +130,16 @@ export default function UpdatePetForm() {
 
                 <Field
                   as="select"
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
                   id="pet_type"
                   name="pet_type"
-=======
-                  id="petType"
-                  name="petType"
->>>>>>> 5c2ccd2 (feat: edit update pet form)
-=======
-                  id="pet_type"
-                  name="pet_type"
->>>>>>> b87b3ee (feat: create api get pet)
-=======
-                  id="type"
-                  name="type"
->>>>>>> 8ff4edd (refactor: edit update pet form)
                   validate={validateRequired}
                   className="select select-bordered w-full outline-none ring-0 border-[#DCDFED] text-[#7B7E8F] font-normal text-[16px]"
                 >
                   <option value="">Select your pet type</option>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b87b3ee (feat: create api get pet)
                   <option value="dog">Dog</option>
                   <option value="cat">Cat</option>
                   <option value="bird">Bird</option>
                   <option value="rabbit">Rabbit</option>
-<<<<<<< HEAD
-=======
-                  <option value="Dog">Dog</option>
-                  <option value="Cat">Cat</option>
-                  <option value="Bird">Bird</option>
-                  <option value="Rabbit">Rabbit</option>
->>>>>>> 5c2ccd2 (feat: edit update pet form)
-=======
->>>>>>> b87b3ee (feat: create api get pet)
                 </Field>
               </div>
               {/* Breed */}
@@ -419,8 +185,8 @@ export default function UpdatePetForm() {
                   className="select select-bordered w-full outline-none ring-0 border-[#DCDFED] text-[#7B7E8F] font-normal text-[16px]"
                 >
                   <option value="">Select sex of your pet</option>
-                  <option value="male">Male</option>
-                  <option value="female">Female</option>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
                 </Field>
               </div>
               {/* Age */}
@@ -499,78 +265,38 @@ export default function UpdatePetForm() {
 
             {/* About */}
             <div className="flex flex-col w-full">
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b87b3ee (feat: create api get pet)
               <label
                 htmlFor="description"
                 className="text-[16px] font-bold pb-1"
               >
                 Description
-<<<<<<< HEAD
               </label>
               <Field
                 as="textarea"
                 id="description"
                 name="description"
-=======
-              <label htmlFor="about" className="text-[16px] font-bold pb-1">
-                About
-              </label>
-              <Field
-                as="textarea"
-                id="about"
-                name="about"
->>>>>>> 5c2ccd2 (feat: edit update pet form)
-=======
-              </label>
-              <Field
-                as="textarea"
-                id="description"
-                name="description"
->>>>>>> b87b3ee (feat: create api get pet)
                 className="border-[#DCDFED] text-[#7B7E8F] rounded-lg h-[140px]"
                 placeholder="Describe more about your pet..."
               />
             </div>
 
             {/* delete pet */}
-            <button className="flex gap-2 items-center" onClick={onDelete}>
+            <div className="flex gap-2 items-center cursor-pointer">
               <Image
                 src="/assets/icons/icon-bin.svg"
-<<<<<<< HEAD
-<<<<<<< HEAD
                 alt="Delete Pet Icon"
-=======
-                alt="Dummy Pet Image"
->>>>>>> 5c2ccd2 (feat: edit update pet form)
-=======
-                alt="Delete Pet Icon"
->>>>>>> b87b3ee (feat: create api get pet)
                 width={18}
                 height={20}
               />
               <p className="text-ps-orange-500 text-b2">Delete Pet</p>
-            </button>
+            </div>
 
             {/* Buttons */}
             <div className="flex flex-wrap gap-4 justify-between">
               <button
                 type="button"
                 className="w-[127px] bg-ps-orange-100 text-ps-orange-500 text-[16px] font-bold rounded-full tracking-wide h-[48px]"
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
                 onClick={() => router.back()} // Navigate back on cancel
-=======
->>>>>>> 5c2ccd2 (feat: edit update pet form)
-=======
-                onClick={() => router.back()} // Navigate back on cancel
->>>>>>> b87b3ee (feat: create api get pet)
-=======
-                onClick={() => router.back()}
->>>>>>> 8ff4edd (refactor: edit update pet form)
               >
                 Cancel
               </button>
