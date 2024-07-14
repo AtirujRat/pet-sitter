aaaebd7357993ae427522d2195bf6515dab14151
boschaiyadej <130572668+boschaiyadej@users.noreply.github.com>
2024-07-14 13:16:52 +0700

    feat: set path of pet list

--- components/owners/yourpet/CreatePetForm.js
+++ components/owners/yourpet/CreatePetForm.js
@@ -1,268 +1,67 @@
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import axios from "axios";
-import { useRouter } from "next/router";
-import Image from "next/image";
+import PetCard from "./PetCard";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const API_URL = "/api/owners";
 
-export default function CreatePetForm() {
+export default function PetList() {
   const router = useRouter();
   const { id } = router.query;
+  const [pets, setPets] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const initialValues = {
-    name: "",
-    type: "",
-    breed: "",
-    sex: "",
-    age: "",
-    color: "",
-    weight: "",
-    description: "",
-  };
-  const validateRequired = (value) => {
-    let error;
-    if (!value || value === "") {
-      error = "Required";
-    }
-    return error;
-  };
+  useEffect(() => {
+    const fetchPets = async () => {
+      setLoading(true);
+      setError(null);
 
-  const onSubmit = async (values, actions) => {
-    try {
-      const response = await axios.post(`${API_URL}/${id}`, {
-        ...values,
-        owner_id: id,
-      });
+      try {
+        if (id) {
+          const response = await axios.get(`${API_URL}/${id}`);
+          setPets(response.data);
+        }
 
-      console.log("Response:", response.data);
-      router.push(`/owners/${id}/yourpet`);
-    } catch (error) {
-      console.error("Error creating pet:", error);
-    } finally {
-      actions.setSubmitting(false);
-    }
-  };
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching pets:", error.message);
+        setError("Error fetching pets");
+        setLoading(false);
+      }
+    };
+
+    fetchPets();
+  }, [id]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit}>
-      {({ isSubmitting, errors, touched }) => (
-        <Form className="w-[75%] h-fit shadow-lg rounded-xl bg-ps-white p-10">
-          <div className="flex flex-col gap-10">
-            <p className="flex text-h3 gap-2">Your Pet</p>
-            {/* Image */}
-            <div className="cursor-pointer w-fit h-fit">
-              <Image
-                src="/assets/pets/pet-dummy.svg"
-                alt="Dummy Pet Image"
-                width={240}
-                height={240}
-              />
-            </div>
-            {/* Pet Name */}
-            <div className="max-sm:hidden flex flex-col">
-              <label htmlFor="name" className="text-[16px] font-bold pb-1 flex">
-                Pet Name*
-                <ErrorMessage
-                  name="name"
-                  component="div"
-                  className="text-ps-red text-b3"
-                />
-              </label>
-              <Field
-                type="text"
-                id="name"
-                name="name"
-                validate={validateRequired}
-                className="border-[#DCDFED] text-[#7B7E8F] rounded-lg"
-                placeholder="John Wick"
-              />
-            </div>
-
-            <div className="flex justify-between">
-              {/* Pet Type */}
-              <div className="flex flex-col w-[48%]">
-                <label htmlFor="type" className="flex text-[16px] font-bold">
-                  Pet Type*
-                  <ErrorMessage
-                    name="type"
-                    component="div"
-                    className="text-ps-red text-b3"
-                  />
-                </label>
-
-                <Field
-                  as="select"
-                  id="type"
-                  name="type"
-                  validate={validateRequired}
-                  className="select select-bordered w-full outline-none ring-0 border-[#DCDFED] text-[#7B7E8F] font-normal text-[16px]"
-                >
-                  <option value="">Select your pet type</option>
-                  <option value="dog">Dog</option>
-                  <option value="cat">Cat</option>
-                  <option value="bird">Bird</option>
-                  <option value="rabbit">Rabbit</option>
-                </Field>
-              </div>
-              {/* Breed */}
-              <div className="flex flex-col w-[48%]">
-                <label
-                  htmlFor="breed"
-                  className="flex text-[16px] font-bold pb-1"
-                >
-                  Breed*
-                  <ErrorMessage
-                    name="breed"
-                    component="div"
-                    className="text-ps-red text-b3"
-                  />
-                </label>
-                <Field
-                  type="text"
-                  id="breed"
-                  name="breed"
-                  validate={validateRequired}
-                  className="border-[#DCDFED] text-[#7B7E8F] rounded-lg"
-                  placeholder="Breed of your pet"
-                />
-              </div>
-            </div>
-
-            <div className="flex justify-between">
-              {/* Sex */}
-              <div className="flex flex-col w-[48%]">
-                <label htmlFor="sex" className="flex text-[16px] font-bold">
-                  Sex*
-                  <ErrorMessage
-                    name="sex"
-                    component="div"
-                    className="text-ps-red text-b3"
-                  />
-                </label>
-                <Field
-                  as="select"
-                  id="sex"
-                  name="sex"
-                  validate={validateRequired}
-                  className="select select-bordered w-full outline-none ring-0 border-[#DCDFED] text-[#7B7E8F] font-normal text-[16px]"
-                >
-                  <option value="">Select sex of your pet</option>
-                  <option value="male">Male</option>
-                  <option value="female">Female</option>
-                </Field>
-              </div>
-              {/* Age */}
-              <div className="flex flex-col w-[48%]">
-                <label
-                  htmlFor="age"
-                  className="flex text-[16px] font-bold pb-1"
-                >
-                  Age (Months)*
-                  <ErrorMessage
-                    name="age"
-                    component="div"
-                    className="text-ps-red text-b3"
-                  />
-                </label>
-                <Field
-                  type="number"
-                  id="age"
-                  name="age"
-                  validate={validateRequired}
-                  className="border-[#DCDFED] text-[#7B7E8F] rounded-lg"
-                  placeholder="Age of your pet"
-                  min="1"
-                />
-              </div>
-            </div>
-
-            <div className="flex justify-between">
-              {/* Color */}
-              <div className="flex flex-col w-[48%]">
-                <label
-                  htmlFor="color"
-                  className="flex text-[16px] font-bold pb-1"
-                >
-                  Color*
-                  <ErrorMessage
-                    name="color"
-                    component="div"
-                    className="text-ps-red text-b3"
-                  />
-                </label>
-                <Field
-                  type="text"
-                  id="color"
-                  name="color"
-                  validate={validateRequired}
-                  className="border-[#DCDFED] text-[#7B7E8F] rounded-lg"
-                  placeholder="Describe color of your pet"
-                />
-              </div>
-              {/* Weight */}
-              <div className="flex flex-col w-[48%]">
-                <label
-                  htmlFor="weight"
-                  className="flex text-[16px] font-bold pb-1"
-                >
-                  Weight (Kilogram)*
-                  <ErrorMessage
-                    name="weight"
-                    component="div"
-                    className="text-ps-red text-b3"
-                  />
-                </label>
-                <Field
-                  type="number"
-                  id="weight"
-                  name="weight"
-                  validate={validateRequired}
-                  className="border-[#DCDFED] text-[#7B7E8F] rounded-lg"
-                  placeholder="Weight of your pet"
-                  min="1"
-                  step="0.1"
-                />
-              </div>
-            </div>
-
-            {/* About */}
-            <div className="flex flex-col w-full">
-              <label
-                htmlFor="description"
-                className="text-[16px] font-bold pb-1"
-              >
-                About
-              </label>
-              <Field
-                as="textarea"
-                id="description"
-                name="description"
-                className="border-[#DCDFED] text-[#7B7E8F] rounded-lg h-[140px]"
-                placeholder="Describe more about your pet..."
-              />
-            </div>
-
-            {/* Buttons */}
-            <div className="flex flex-wrap gap-4 justify-between">
-              <button
-                type="button"
-                className="w-[127px] bg-ps-orange-100 text-ps-orange-500 text-[16px] font-bold rounded-full tracking-wide h-[48px]"
-                onClick={() => router.back()}
-              >
-                Cancel
-              </button>
-              <button
-                type="submit"
-                className="w-[127px] bg-ps-orange-500 text-ps-white text-[16px] font-bold rounded-full tracking-wide h-[48px]"
-                disabled={isSubmitting}
-              >
-                {isSubmitting ? "Creating..." : "Create Pet"}
-              </button>
-            </div>
-          </div>
-        </Form>
-      )}
-    </Formik>
+    <section className="w-[75%] min-h-[824px] h-fit shadow-lg rounded-xl bg-ps-white">
+      <div className="flex flex-col justify-center p-10 gap-10">
+        <div className="flex justify-between items-center">
+          <p className="text-h3">Your Pet</p>
+          <Link href={`/owners/${id}/yourpet/create`}>
+            <button className="w-[127px] bg-ps-orange-500 text-ps-white text-[16px] font-bold rounded-full tracking-wide h-[48px]">
+              Create Pet
+            </button>
+          </Link>
+        </div>
+        <div className="flex flex-wrap gap-4 justify-stretch">
+          {pets.map((pet) => (
+            <Link key={pet.id} href={`/owners/${id}/yourpet/${pet.id}`}>
+              <PetCard key={pet.id} name={pet.name} type={pet.type} />
+            </Link>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
