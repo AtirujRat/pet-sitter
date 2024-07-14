b500a49a963830380f5040b78994962d2941d736
wiKanok <kanokwan1401.ks@gmail.com>
2024-07-15 06:20:57 +0700

    feat:create form manage profile section basic information

--- pages/api/sitters/[id].js
+++ pages/api/sitters/[id].js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
       let { data: sitters, error } = await supabase
         .from("sitters")
         .select(
-          "id, full_name, trade_name, experience, introduction, services, place_description, profile_image_url, sitters_images(image_url), pet_types(id,pet_type), bookings(reviews!inner(rating))"
+          "*, sitters_images(image_url), pet_types(id,pet_type), bookings(reviews!inner(rating))"
         )
         .eq("id", id);
 
@@ -20,6 +20,86 @@ export default async function handler(req, res) {
       });
     } catch (error) {
       return res.status(500).json({ error: error.message });
+    }
+  }
+
+  if (req.method === "PUT") {
+    const { id } = req.query;
+    const reqBody = { ...req.body, updated_at: new Date() };
+
+    try {
+      // ตรวจสอบว่าแถวข้อมูลมีอยู่หรือไม่
+      const { data: existingData, error } = await supabase
+        .from("sitters")
+        .select("*")
+        .eq("id", id);
+
+      if (error) {
+        throw error;
+      }
+
+      if (existingData.length > 0) {
+        // แถวข้อมูลมีอยู่แล้ว, ทำการอัปเดต
+        const { data, error } = await supabase
+          .from("sitters")
+          .update({
+            email: reqBody.email,
+            password: reqBody.password,
+            phone_number: reqBody.phone_number,
+            profile_image_url: reqBody.profile_image_url,
+            full_name: reqBody.full_name,
+            experience: reqBody.experience,
+            introduction: reqBody.introduction,
+            bank_id: reqBody.bank_id,
+            account_number: reqBody.account_number,
+            trade_name: reqBody.trade_name,
+            place_description: reqBody.place_description,
+            member_status: reqBody.member_status,
+            sitter_address_id: reqBody.sitter_address_id,
+            updated_at: reqBody.updated_at,
+            last_logged_in: reqBody.last_logged_in,
+          })
+          .eq("id", id)
+          .select();
+
+        if (error) {
+          throw error;
+        }
+
+        res.status(200).json(data);
+      } else {
+        // แถวข้อมูลไม่มี, ทำการเพิ่มข้อมูล
+        const { data, error } = await supabase
+          .from("sitters")
+          .upsert({
+            id,
+            email: reqBody.email,
+            password: reqBody.password,
+            phone_number: reqBody.phone_number,
+            profile_image_url: reqBody.profile_image_url,
+            full_name: reqBody.full_name,
+            experience: reqBody.experience,
+            introduction: reqBody.introduction,
+            bank_id: reqBody.bank_id,
+            account_number: reqBody.account_number,
+            trade_name: reqBody.trad_ename,
+            place_description: reqBody.place_description,
+            member_status: reqBody.member_status,
+            sitter_address_id: reqBody.sitter_address_id,
+            created_at: reqBody.created_at,
+            updated_at: reqBody.updated_at,
+            last_logged_in: reqBody.last_logged_in,
+          })
+          .select();
+
+        if (error) {
+          throw error;
+        }
+
+        res.status(200).json(data);
+      }
+    } catch (error) {
+      res.status(400).json({ error: error.message });
     }
   } else {
     return res.status(405).json({ error: "Method Not Available" });
--- pages/sitters/[id]/profile/index.js
+++ pages/sitters/[id]/profile/index.js
@@ -4,18 +4,48 @@ import NavBarSitter from "@/components/sitters/profile/NavbarSitter";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import defaultImage from "../../../../public/assets/navbar/usermock.svg";
 
 export default function SitterManageProfile() {
   const router = useRouter();
   const { id } = router.query;
 
   const [profile, setProfile] = useState(null);
+  const [preview, setPreview] = useState(null);
+
+  const ImageChange = ({ setPreview }) => {
+    const { setFieldValue } = useFormikContext();
+    const handleImageChange = async (event) => {
+      const file = event.currentTarget.files[0];
+      setFieldValue("image", file);
+
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result);
+      };
+      if (file) {
+        reader.readAsDataURL(file);
+      }
+    };
+
+    return (
+      <input
+        className="w-[60px] h-[60px] opacity-0"
+        type="file"
+        name="image"
+        onChange={handleImageChange}
+        accept="image/*"
+      />
+    );
+  };
 
   const GetProfile = async () => {
     try {
       if (id) {
         const response = await axios.get(`/api/sitters/${id}`);
-        setProfile(response.data);
+        console.log("Response Data:", response.data);
+        setProfile(response.data.data[0]);
+        // console.log(response.data.data[0]);
       }
     } catch (error) {
       console.error("Error fetching profile data:", error);
@@ -35,14 +65,18 @@ export default function SitterManageProfile() {
       <SideBarSitter />
       <div className="w-full flex-col">
         <NavBarSitter
-          profileImage={profile.profile_image_url}
+          profileImage={
+            profile.profile_image_url
+              ? profile.profile_image_url
+              : defaultImage.src
+          }
           fullName={profile.full_name}
         />
         <div className="bg-ps-gray-200 h-full flex flex-col gap-6 p-10">
           <div className="text-h3">
             <p>Pet Sitter Profile</p>
           </div>
-          <SitterProfileForm />
+          <SitterProfileForm profile={{ ...profile }} />
         </div>
       </div>
     </div>
