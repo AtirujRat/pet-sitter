43d0d7cd4b6af88596b4eceea59fb0c2dbf6f63d
boschaiyadej <130572668+boschaiyadej@users.noreply.github.com>
2024-07-14 13:16:52 +0700

    feat: set path of pet list

--- components/owners/yourpet/PetList.js
+++ components/owners/yourpet/PetList.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { supabase } from "@/utils/supabase";
@@ -63,12 +64,31 @@ export default function YourPage() {
   if (error) {
     return <div>Error: {error}</div>;
   }
+=======
+import { useRouter } from "next/router";
+
+export default function YourPet() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  if (!id) {
+    return <div>Loading...</div>;
+  }
+
+  const pets = [
+    { name: "Bubba", type: "Dog" },
+    { name: "Daisy", type: "Dog" },
+    { name: "I Som", type: "Cat" },
+    { name: "Noodle Bird", type: "Bird" },
+  ];
+>>>>>>> ce812ed (feat: set path of pet list)
 
   return (
     <section className="w-[75%] min-h-[824px] h-fit shadow-lg rounded-xl bg-ps-white">
       <div className="flex flex-col justify-center p-10 gap-10">
         <div className="flex justify-between items-center">
           <p className="text-h3">Your Pet</p>
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -80,15 +100,24 @@ export default function YourPage() {
 >>>>>>> f6fd319 (feat: edit update pet form)
           <Link href="/owners/yourpet/create">
 >>>>>>> af253ba (feat: update create pet form)
+=======
+          <Link href={`/owners/${id}/yourpet/create`}>
+>>>>>>> ce812ed (feat: set path of pet list)
             <button className="w-[127px] bg-ps-orange-500 text-ps-white text-[16px] font-bold rounded-full tracking-wide h-[48px]">
               Create Pet
             </button>
           </Link>
         </div>
         <div className="flex flex-wrap gap-4 justify-stretch">
+<<<<<<< HEAD
           {pets.map((pet) => (
             <Link key={pet.id} href={`/owners/${id}/yourpet/${pet.id}`}>
               <PetCard key={pet.id} name={pet.name} type={pet.pet_type} />
+=======
+          {pets.map((pet, index) => (
+            <Link key={pet.id} href={`/owners/${id}/yourpet/${pet.id}/update`}>
+              <PetCard key={index} name={pet.name} type={pet.type} />
+>>>>>>> ce812ed (feat: set path of pet list)
             </Link>
           ))}
         </div>
