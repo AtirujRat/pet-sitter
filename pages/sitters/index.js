e4a740c3a1c3130f998e1e0dd2bd5138f19603d6
Ja <ja.techup@gmail.com>
2024-07-15 00:25:41 +0700

    refactor: adjust components on sitters page

--- pages/sitters/index.js
+++ pages/sitters/index.js
@@ -1,10 +1,7 @@
 import SittersList from "@/components/sitters/SittersList";
-import ViewTypeButton from "@/components/sitters/ViewTypeButton";
+import PageTitleDesktop from "@/components/sitters/PageTitleDesktop";
 import SearchBarSitter from "@/components/sitters/SearchBarSitter";
-import listInactive from "/public/assets/sitters/icon-list-inactive.svg";
-import mapInactive from "/public/assets/sitters/icon-map-inactive.svg";
-import goUp from "/public/assets/sitters/icon-up.svg";
-import Image from "next/image";
+import PageTitleMobile from "@/components/sitters/PageTitleMobile";
 import Pagination from "@/components/Pagination";
 import axios from "axios";
 import { useState, useEffect, createContext } from "react";
@@ -79,44 +76,11 @@ export default function Sitters() {
     >
       <section className="w-full flex justify-center bg-ps-gray-100 sm:py-8 pt-4 lg:pb-32">
         <div className="page-container w-full sm:px-20 max-w-[1440px] flex flex-col items-center">
-          {/* Desktop: Search Title */}
-          <div className="title-container max-lg:hidden flex justify-between w-full my-10">
-            <div className="text-h3 text-ps-gray-600">
-              Search For Pet Sitter
-            </div>
-            <div className="view-type flex gap-2.5 ">
-              <ViewTypeButton type="List" src={listInactive} alt="list-view" />
-              <ViewTypeButton type="Map" src={mapInactive} alt="map-view" />
-            </div>
-          </div>
+          <PageTitleDesktop />
           <div className="content-container flex gap-8 max-lg:flex-col lg:mb-11">
             <SearchBarSitter />
-            {/* Mobile: Search Title */}
-            <div className="title-container lg:hidden flex-col flex items-center max-sm:px-5">
-              <div className="text-h4 w-fit text-ps-gray-600 mb-3">
-                Search For Pet Sitter
-              </div>
-              <div className="view-type flex gap-2.5 w-full">
-                <ViewTypeButton
-                  type="List"
-                  src={listInactive}
-                  alt="list-view"
-                />
-                <ViewTypeButton type="Map" src={mapInactive} alt="map-view" />
-              </div>
-            </div>
+            <PageTitleMobile />
             <SittersList sitters={sitters} />
-            <button
-              className="lg:hidden rounded-full px-3 py-3 bg-ps-orange-100 justify-center items-center flex w-16 h-16 self-end max-sm:mx-5 relative -top-14"
-              onClick={() => {
-                window.scrollTo({
-                  top: 0,
-                  behavior: "smooth",
-                });
-              }}
-            >
-              <Image src={goUp} width={24} height={24} alt="Go up" />
-            </button>
           </div>
           <Pagination />
         </div>
