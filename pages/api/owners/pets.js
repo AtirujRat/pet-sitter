3ea23dcd923a7adcdc7620035988eecdcc8920ed
boschaiyadej <130572668+boschaiyadej@users.noreply.github.com>
2024-07-14 14:40:16 +0700

    feat: create pet

--- pages/api/owners/pets.js
+++ pages/api/owners/pets.js
@@ -53,6 +53,7 @@ export default async function handler(req, res) {
     try {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
       const { data, error } = await supabase
         .from("pets")
         .update(values)
@@ -92,6 +93,25 @@ export default async function handler(req, res) {
 =======
       res.status(500).json({ message: "Error updating pet", error });
 >>>>>>> 6fecf16 (feat: create api update pet)
+=======
+      const { id, ...updatedPet } = req.body;
+
+      const { data: updatedData, error: updateError } = await supabase
+        .from("pets")
+        .update(updatedPet)
+        .eq("id", id);
+
+      if (updateError) {
+        throw new Error("Error updating pet in the database");
+      }
+
+      return res
+        .status(200)
+        .json({ message: "Pet updated successfully", pet: updatedData[0] });
+    } catch (error) {
+      console.error("Error updating pet:", error);
+      return res.status(500).json({ message: "Failed to update pet" });
+>>>>>>> 01989d5 (feat: create pet)
     }
   } else if (req.method === "DELETE") {
     try {
