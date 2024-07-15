42dc4554311df0ab47f15501e79be80f86e795ff
PNKim <paljapon.nait@gmail.com>
2024-07-15 16:43:59 +0700

    feat: map can search with custom address

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,14 @@
 // import HeaderHomes from "@/components/home/HeaderHome";
 // import ContentHomePage from "@/components/home/ContentHomePage";
 // import FindAPetSister from "@/components/home/FindAPetSister";
-import PlaceSearch from "@/components/PlaceSearch";
-import Map from "@/components/Map";
+import AddressForm from "@/components/AddressForm";
 
 export default function Home() {
   return (
     <div className="flex">
       <div>
-        <PlaceSearch />
+        <AddressForm />
       </div>
-      <Map />
     </div>
     // <section className="w-full flex-col items-center lg:pt-20 pt-10 overflow-hidden ">
     //   <HeaderHomes />
