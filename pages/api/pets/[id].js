834db4ed70dc9c9b130409f1cb046cc8f18ff164
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-07-15 19:10:04 +0700

    refactor: edit update pet form

--- pages/api/pets/[id].js
+++ pages/api/pets/[id].js
@@ -5,83 +5,30 @@ export default async function handler(req, res) {
 
   if (req.method === "GET") {
     try {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 9dc1fd7 (refactor: update api pets)
-=======
->>>>>>> 9dc1fd7 (refactor: update api pets)
-      const { data, error } = await supabase
-        .from("pets")
-        .select("*")
-        .eq("id", id);
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-      if (error) {
-        throw error;
-      }
-
-      if (data.length > 0) {
-        return res.status(200).json(data[0]);
-      } else {
-        return res.status(404).json({ message: "Pet not found" });
-      }
-    } catch (error) {
-      console.error("Error fetching pet:", error.message);
-      return res.status(500).json({ message: "Error fetching pet" });
-=======
-=======
->>>>>>> 8ff4edd (refactor: edit update pet form)
       const getPetsByOwner = async (ownerId) => {
         const { data, error } = await supabase
           .from("pets")
           .select("*")
           .eq("owner_id", ownerId);
-<<<<<<< HEAD
-=======
->>>>>>> 9dc1fd7 (refactor: update api pets)
 
-      if (error) {
-        throw error;
-      }
+        if (error) {
+          throw error;
+        }
 
-      if (data.length > 0) {
-        return res.status(200).json(data[0]);
+        return data;
+      };
+
+      const pets = await getPetsByOwner(id);
+
+      if (pets.length > 0) {
+        return res.status(200).json(pets);
       } else {
-        return res.status(404).json({ message: "Pet not found" });
+        return res
+          .status(404)
+          .json({ message: "No pets found for this owner." });
       }
     } catch (error) {
-<<<<<<< HEAD
       return res.status(500).json({ error: error.message });
->>>>>>> 8ff4edd (refactor: edit update pet form)
-=======
-      console.error("Error fetching pet:", error.message);
-      return res.status(500).json({ message: "Error fetching pet" });
->>>>>>> 9dc1fd7 (refactor: update api pets)
-=======
-=======
->>>>>>> 9dc1fd7 (refactor: update api pets)
-
-      if (error) {
-        throw error;
-      }
-
-      if (data.length > 0) {
-        return res.status(200).json(data[0]);
-      } else {
-        return res.status(404).json({ message: "Pet not found" });
-      }
-    } catch (error) {
-<<<<<<< HEAD
-      return res.status(500).json({ error: error.message });
->>>>>>> 8ff4edd (refactor: edit update pet form)
-=======
-      console.error("Error fetching pet:", error.message);
-      return res.status(500).json({ message: "Error fetching pet" });
->>>>>>> 9dc1fd7 (refactor: update api pets)
     }
   } else if (req.method === "PUT") {
     try {
@@ -89,23 +36,7 @@ export default async function handler(req, res) {
         req.body;
 
       if (!name || !type || !breed || !sex || !age || !color || !weight) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        return res.status(400).json({ message: "Missing required fields" });
-=======
         return res.status(400).json({ error: "Missing required fields" });
->>>>>>> 8ff4edd (refactor: edit update pet form)
-=======
-        return res.status(400).json({ message: "Missing required fields" });
->>>>>>> 9dc1fd7 (refactor: update api pets)
-=======
-        return res.status(400).json({ error: "Missing required fields" });
->>>>>>> 8ff4edd (refactor: edit update pet form)
-=======
-        return res.status(400).json({ message: "Missing required fields" });
->>>>>>> 9dc1fd7 (refactor: update api pets)
       }
 
       const { data, error } = await supabase
@@ -121,40 +52,11 @@ export default async function handler(req, res) {
           description,
           updated_at: new Date(),
         })
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         .eq("id", id)
         .single();
 
       if (error) {
         throw error;
-=======
-=======
->>>>>>> 8ff4edd (refactor: edit update pet form)
-        .eq("id", id);
-
-      if (error) {
-        return res.status(500).json({ error: error.message });
-<<<<<<< HEAD
->>>>>>> 8ff4edd (refactor: edit update pet form)
-=======
-        .eq("id", id)
-        .single();
-
-      if (error) {
-        throw error;
->>>>>>> 9dc1fd7 (refactor: update api pets)
-=======
->>>>>>> 8ff4edd (refactor: edit update pet form)
-=======
-        .eq("id", id)
-        .single();
-
-      if (error) {
-        throw error;
->>>>>>> 9dc1fd7 (refactor: update api pets)
       }
 
       return res
@@ -162,15 +64,7 @@ export default async function handler(req, res) {
         .json({ message: "Pet updated successfully", data });
     } catch (error) {
       console.error("Error updating pet:", error.message);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 9dc1fd7 (refactor: update api pets)
-=======
->>>>>>> 9dc1fd7 (refactor: update api pets)
-      return res.status(500).json({ message: "Error updating pet" });
+      return res.status(500).json({ error: "Error updating pet" });
     }
   } else if (req.method === "DELETE") {
     try {
@@ -183,33 +77,10 @@ export default async function handler(req, res) {
       return res.status(200).json({ message: "Pet deleted successfully" });
     } catch (error) {
       console.error("Error deleting pet:", error.message);
-      return res.status(500).json({ message: "Error deleting pet" });
-<<<<<<< HEAD
-<<<<<<< HEAD
+      return res.status(500).json({ error: "Error deleting pet" });
     }
   } else {
     res.setHeader("Allow", ["GET", "PUT", "DELETE"]);
-=======
-=======
->>>>>>> 8ff4edd (refactor: edit update pet form)
-      return res.status(500).json({ error: "Error updating pet" });
-    }
-  } else {
-    res.setHeader("Allow", ["GET", "PUT"]);
-<<<<<<< HEAD
->>>>>>> 8ff4edd (refactor: edit update pet form)
-=======
-    }
-  } else {
-    res.setHeader("Allow", ["GET", "PUT", "DELETE"]);
->>>>>>> 9dc1fd7 (refactor: update api pets)
-=======
->>>>>>> 8ff4edd (refactor: edit update pet form)
-=======
-    }
-  } else {
-    res.setHeader("Allow", ["GET", "PUT", "DELETE"]);
->>>>>>> 9dc1fd7 (refactor: update api pets)
     return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
--- pages/api/pets/index.js
+++ pages/api/pets/index.js
@@ -1,444 +1,95 @@
 import { supabase } from "@/utils/supabase";
 
 export default async function handler(req, res) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-  const { path } = req.query;
-  const [id, ...rest] = path;
-
   if (req.method === "GET") {
     try {
-      if (rest.length > 0) {
-        const petId = rest[0];
+      const { data: pets, error: fetchError } = await supabase
+        .from("pets")
+        .select("*");
 
-        const { data: pet, error } = await supabase
-          .from("pets")
-          .select("*")
-          .eq("owner_id", id)
-          .eq("id", petId)
-          .single();
-
-        if (error) {
-          throw error;
-        }
-
-        if (pet) {
-          return res.status(200).json(pet);
-        } else {
-          return res.status(404).json({ message: "Pet not found" });
-        }
-      } else {
-        const { data: pets, error } = await supabase
-          .from("pets")
-          .select("*")
-          .eq("owner_id", id);
-
-        if (error) {
-          throw error;
-        }
-
-        return res.status(200).json(pets);
+      if (fetchError) {
+        throw new Error("Error fetching pets from the database");
       }
+
+      return res.status(200).json({ pets });
     } catch (error) {
-      console.error("Error fetching pet:", error.message);
-      return res.status(500).json({ message: "Error fetching pet" });
+      console.error("Error fetching pets:", error);
+      return res.status(500).json({ message: "Failed to fetch pets" });
     }
   } else if (req.method === "POST") {
     try {
-      const pet = req.body;
+      const newPet = req.body;
 
-      const { data: newPet, error } = await supabase.from("pets").insert([
-        {
-          owner_id: id,
-          profile_image_url: pet.profile_image_url || "",
-          name: pet.name,
-          type: pet.type,
-          breed: pet.breed,
-          age: pet.age,
-          sex: pet.sex,
-          color: pet.color,
-          weight: pet.weight,
-          description: pet.description,
-          status: "Active",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-      ]);
-
-      if (error) {
-        throw error;
+      if (!newPet.owner_id) {
+        return res.status(400).json({ message: "Owner ID is required" });
       }
 
-      return res
-        .status(201)
-        .json({ message: "Pet created successfully", data: newPet[0] });
-    } catch (error) {
-      console.error("Error creating pet:", error.message);
-      return res.status(500).json({ message: "Error creating pet" });
-    }
-  } else if (req.method === "PUT") {
-    try {
-      const petId = rest[0];
-      const { name, type, breed, sex, age, color, weight, description } =
-        req.body;
-
-      const { data: updatedPet, error } = await supabase
+      const { data: insertedPet, error: insertError } = await supabase
         .from("pets")
-        .update({
-          name,
-          type,
-          breed,
-          sex,
-          age,
-          color,
-          weight,
-          description,
-          updated_at: new Date(),
-        })
-        .eq("owner_id", id)
-        .eq("id", petId)
-        .single();
+        .insert({
+          owner_id: newPet.owner_id,
+          name: newPet.petName,
+          pet_type: newPet.petType,
+          breed: newPet.breed,
+          age: newPet.age,
+          gender: newPet.sex,
+          color: newPet.color,
+          weight: newPet.weight,
+          description: newPet.about,
+        });
 
-      if (error) {
-        throw error;
-      }
-
-      if (!updatedPet) {
-        return res.status(404).json({ message: "Pet not found" });
+      if (insertError) {
+        throw new Error("Error inserting pet into the database");
       }
 
       return res
         .status(200)
-        .json({ message: "Pet updated successfully", data: updatedPet });
+        .json({ message: "Pet created successfully", pet: insertedPet[0] });
     } catch (error) {
-      console.error("Error updating pet:", error.message);
-      return res.status(500).json({ message: "Error updating pet" });
+      console.error("Error creating pet:", error);
+      return res.status(500).json({ message: "Failed to create pet" });
     }
-  } else if (req.method === "DELETE") {
+  } else if (req.method === "PUT") {
     try {
-      const petId = rest[0];
+      const { id, ...updatedPet } = req.body;
+      const { ownerId, petId } = updatedPet; // Assuming these are passed in the body
 
-      const { error } = await supabase
+      const { data, error } = await supabase
         .from("pets")
-        .delete()
-        .eq("owner_id", id)
+        .update(updatedPet)
+        .eq("owner_id", ownerId)
         .eq("id", petId);
 
       if (error) {
         throw error;
       }
 
-      return res.status(200).json({ message: "Pet deleted successfully" });
-    } catch (error) {
-      console.error("Error deleting pet:", error.message);
-      return res.status(500).json({ message: "Error deleting pet" });
-    }
-  } else {
-    res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
-=======
-=======
->>>>>>> 8ff4edd (refactor: edit update pet form)
-  if (req.method === "POST") {
-    const pet = req.body;
-    const { data, error } = await supabase.from("pets").insert([
-      {
-        owner_id: pet.owner_id,
-        profile_image_url: pet.profile_image_url || "",
-        name: pet.name,
-        type: pet.type,
-        breed: pet.breed,
-        age: pet.age,
-        sex: pet.sex,
-        color: pet.color,
-        weight: pet.weight,
-        description: pet.description,
-        status: "Available",
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-    ]);
-<<<<<<< HEAD
-=======
-  const { path } = req.query;
-  const [id, ...rest] = path;
->>>>>>> 9dc1fd7 (refactor: update api pets)
-
-  if (req.method === "GET") {
-    try {
-      if (rest.length > 0) {
-        const petId = rest[0];
-
-        const { data: pet, error } = await supabase
-          .from("pets")
-          .select("*")
-          .eq("owner_id", id)
-          .eq("id", petId)
-          .single();
-
-        if (error) {
-          throw error;
-        }
-
-        if (pet) {
-          return res.status(200).json(pet);
-        } else {
-          return res.status(404).json({ message: "Pet not found" });
-        }
-      } else {
-        const { data: pets, error } = await supabase
-          .from("pets")
-          .select("*")
-          .eq("owner_id", id);
-
-        if (error) {
-          throw error;
-        }
-
-        return res.status(200).json(pets);
-      }
-    } catch (error) {
-      console.error("Error fetching pet:", error.message);
-      return res.status(500).json({ message: "Error fetching pet" });
-    }
-  } else if (req.method === "POST") {
-    try {
-      const pet = req.body;
-
-      const { data: newPet, error } = await supabase.from("pets").insert([
-        {
-          owner_id: id,
-          profile_image_url: pet.profile_image_url || "",
-          name: pet.name,
-          type: pet.type,
-          breed: pet.breed,
-          age: pet.age,
-          sex: pet.sex,
-          color: pet.color,
-          weight: pet.weight,
-          description: pet.description,
-          status: "Active",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-      ]);
-
-      if (error) {
-        throw error;
-      }
-
-      return res
-        .status(201)
-        .json({ message: "Pet created successfully", data: newPet[0] });
-    } catch (error) {
-      console.error("Error creating pet:", error.message);
-      return res.status(500).json({ message: "Error creating pet" });
-    }
-  } else if (req.method === "PUT") {
-    try {
-      const petId = rest[0];
-      const { name, type, breed, sex, age, color, weight, description } =
-        req.body;
-
-      const { data: updatedPet, error } = await supabase
-        .from("pets")
-        .update({
-          name,
-          type,
-          breed,
-          sex,
-          age,
-          color,
-          weight,
-          description,
-          updated_at: new Date(),
-        })
-        .eq("owner_id", id)
-        .eq("id", petId)
-        .single();
-
-      if (error) {
-        throw error;
-      }
-
-      if (!updatedPet) {
-        return res.status(404).json({ message: "Pet not found" });
-      }
-
       return res
         .status(200)
-        .json({ message: "Pet updated successfully", data: updatedPet });
+        .json({ message: "Pet updated successfully", data });
     } catch (error) {
-      console.error("Error updating pet:", error.message);
-      return res.status(500).json({ message: "Error updating pet" });
+      console.error("Error updating pet:", error);
+      return res.status(500).json({ message: "Failed to update pet" });
     }
   } else if (req.method === "DELETE") {
     try {
-      const petId = rest[0];
+      const { id } = req.body;
 
-      const { error } = await supabase
+      const { error: deleteError } = await supabase
         .from("pets")
         .delete()
-        .eq("owner_id", id)
-        .eq("id", petId);
+        .eq("id", id);
 
-      if (error) {
-        throw error;
+      if (deleteError) {
+        throw new Error("Error deleting pet from the database");
       }
 
       return res.status(200).json({ message: "Pet deleted successfully" });
     } catch (error) {
-      console.error("Error deleting pet:", error.message);
-      return res.status(500).json({ message: "Error deleting pet" });
+      console.error("Error deleting pet:", error);
+      return res.status(500).json({ message: "Failed to delete pet" });
     }
   } else {
-<<<<<<< HEAD
-    res.setHeader("Allow", ["POST"]);
->>>>>>> 8ff4edd (refactor: edit update pet form)
-=======
-    res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
->>>>>>> 9dc1fd7 (refactor: update api pets)
-=======
-=======
-  const { path } = req.query;
-  const [id, ...rest] = path;
->>>>>>> 9dc1fd7 (refactor: update api pets)
-
-  if (req.method === "GET") {
-    try {
-      if (rest.length > 0) {
-        const petId = rest[0];
-
-        const { data: pet, error } = await supabase
-          .from("pets")
-          .select("*")
-          .eq("owner_id", id)
-          .eq("id", petId)
-          .single();
-
-        if (error) {
-          throw error;
-        }
-
-        if (pet) {
-          return res.status(200).json(pet);
-        } else {
-          return res.status(404).json({ message: "Pet not found" });
-        }
-      } else {
-        const { data: pets, error } = await supabase
-          .from("pets")
-          .select("*")
-          .eq("owner_id", id);
-
-        if (error) {
-          throw error;
-        }
-
-        return res.status(200).json(pets);
-      }
-    } catch (error) {
-      console.error("Error fetching pet:", error.message);
-      return res.status(500).json({ message: "Error fetching pet" });
-    }
-  } else if (req.method === "POST") {
-    try {
-      const pet = req.body;
-
-      const { data: newPet, error } = await supabase.from("pets").insert([
-        {
-          owner_id: id,
-          profile_image_url: pet.profile_image_url || "",
-          name: pet.name,
-          type: pet.type,
-          breed: pet.breed,
-          age: pet.age,
-          sex: pet.sex,
-          color: pet.color,
-          weight: pet.weight,
-          description: pet.description,
-          status: "Active",
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-      ]);
-
-      if (error) {
-        throw error;
-      }
-
-      return res
-        .status(201)
-        .json({ message: "Pet created successfully", data: newPet[0] });
-    } catch (error) {
-      console.error("Error creating pet:", error.message);
-      return res.status(500).json({ message: "Error creating pet" });
-    }
-  } else if (req.method === "PUT") {
-    try {
-      const petId = rest[0];
-      const { name, type, breed, sex, age, color, weight, description } =
-        req.body;
-
-      const { data: updatedPet, error } = await supabase
-        .from("pets")
-        .update({
-          name,
-          type,
-          breed,
-          sex,
-          age,
-          color,
-          weight,
-          description,
-          updated_at: new Date(),
-        })
-        .eq("owner_id", id)
-        .eq("id", petId)
-        .single();
-
-      if (error) {
-        throw error;
-      }
-
-      if (!updatedPet) {
-        return res.status(404).json({ message: "Pet not found" });
-      }
-
-      return res
-        .status(200)
-        .json({ message: "Pet updated successfully", data: updatedPet });
-    } catch (error) {
-      console.error("Error updating pet:", error.message);
-      return res.status(500).json({ message: "Error updating pet" });
-    }
-  } else if (req.method === "DELETE") {
-    try {
-      const petId = rest[0];
-
-      const { error } = await supabase
-        .from("pets")
-        .delete()
-        .eq("owner_id", id)
-        .eq("id", petId);
-
-      if (error) {
-        throw error;
-      }
-
-      return res.status(200).json({ message: "Pet deleted successfully" });
-    } catch (error) {
-      console.error("Error deleting pet:", error.message);
-      return res.status(500).json({ message: "Error deleting pet" });
-    }
-  } else {
-<<<<<<< HEAD
-    res.setHeader("Allow", ["POST"]);
->>>>>>> 8ff4edd (refactor: edit update pet form)
-=======
-    res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
->>>>>>> 9dc1fd7 (refactor: update api pets)
-    return res.status(405).end(`Method ${req.method} Not Allowed`);
+    return res.status(405).json({ message: "Method Not Allowed" });
   }
 }
