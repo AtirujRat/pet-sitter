8ad096b60a442c969760715210290a1e61b14e8d
wiKanok <kanokwan1401.ks@gmail.com>
2024-07-15 07:31:41 +0700

    fix:resolve conflict file

--- pages/sitters/[id]/profile/index.js
+++ pages/sitters/[id]/profile/index.js
@@ -4,18 +4,48 @@ import NavBarSitter from "@/components/sitters/profile/NavbarSitter";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import defaultImage from "../../../../public/assets/navbar/usermock.svg";
 
 export default function SitterManageProfile() {
   const router = useRouter();
   const { id } = router.query;
 
   const [profile, setProfile] = useState(null);
+  const [preview, setPreview] = useState(null);
+
+  const ImageChange = ({ setPreview }) => {
+    const { setFieldValue } = useFormikContext();
+    const handleImageChange = async (event) => {
+      const file = event.currentTarget.files[0];
+      setFieldValue("image", file);
+
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result);
+      };
+      if (file) {
+        reader.readAsDataURL(file);
+      }
+    };
+
+    return (
+      <input
+        className="w-[60px] h-[60px] opacity-0"
+        type="file"
+        name="image"
+        onChange={handleImageChange}
+        accept="image/*"
+      />
+    );
+  };
 
   const GetProfile = async () => {
     try {
       if (id) {
         const response = await axios.get(`/api/sitters/${id}`);
-        setProfile(response.data);
+        console.log("Response Data:", response.data);
+        setProfile(response.data.data[0]);
+        // console.log(response.data.data[0]);
       }
     } catch (error) {
       console.error("Error fetching profile data:", error);
