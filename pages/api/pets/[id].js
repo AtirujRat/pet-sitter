9abf37481aee4fd5e6b92c6327a112157020e3c0
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-07-15 23:19:20 +0700

    refactor: update api pets

--- pages/api/pets/[id].js
+++ pages/api/pets/[id].js
@@ -5,34 +5,23 @@ export default async function handler(req, res) {
 
   if (req.method === "GET") {
     try {
-      const getPetsByOwner = async (ownerId) => {
-        const { data, error } = await supabase
-          .from("pets")
-          .select("*")
-          .eq("owner_id", ownerId);
+      const { data, error } = await supabase
+        .from("pets")
+        .select("*")
+        .eq("id", id);
 
-        if (error) {
-          throw error;
-        }
+      if (error) {
+        throw error;
+      }
 
-        if (data && Array.isArray(data)) {
-          return data;
-        } else {
-          return [];
-        }
-      };
-
-      const pets = await getPetsByOwner(id);
-
-      if (pets.length > 0) {
-        return res.status(200).json(pets);
+      if (data.length > 0) {
+        return res.status(200).json(data[0]);
       } else {
-        return res
-          .status(404)
-          .json({ message: "No pets found for this owner." });
+        return res.status(404).json({ message: "Pet not found" });
       }
     } catch (error) {
-      return res.status(500).json({ error: error.message });
+      console.error("Error fetching pet:", error.message);
+      return res.status(500).json({ message: "Error fetching pet" });
     }
   } else if (req.method === "PUT") {
     try {
@@ -40,7 +29,7 @@ export default async function handler(req, res) {
         req.body;
 
       if (!name || !type || !breed || !sex || !age || !color || !weight) {
-        return res.status(400).json({ error: "Missing required fields" });
+        return res.status(400).json({ message: "Missing required fields" });
       }
 
       const { data, error } = await supabase
@@ -56,10 +45,11 @@ export default async function handler(req, res) {
           description,
           updated_at: new Date(),
         })
-        .eq("id", id);
+        .eq("id", id)
+        .single();
 
       if (error) {
-        return res.status(500).json({ error: error.message });
+        throw error;
       }
 
       return res
@@ -67,10 +57,23 @@ export default async function handler(req, res) {
         .json({ message: "Pet updated successfully", data });
     } catch (error) {
       console.error("Error updating pet:", error.message);
-      return res.status(500).json({ error: "Error updating pet" });
+      return res.status(500).json({ message: "Error updating pet" });
+    }
+  } else if (req.method === "DELETE") {
+    try {
+      const { error } = await supabase.from("pets").delete().eq("id", id);
+
+      if (error) {
+        throw error;
+      }
+
+      return res.status(200).json({ message: "Pet deleted successfully" });
+    } catch (error) {
+      console.error("Error deleting pet:", error.message);
+      return res.status(500).json({ message: "Error deleting pet" });
     }
   } else {
-    res.setHeader("Allow", ["GET", "PUT"]);
+    res.setHeader("Allow", ["GET", "PUT", "DELETE"]);
     return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
