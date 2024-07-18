056fdaad98d1305182f2882075ccdda37f47e221
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-07-18 03:35:31 +0100

    refactor: change postition modal

--- components/modal/Modal.js
+++ components/modal/Modal.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Modal = ({ children }) => {
   return (
-    <div className="fixed flex justify-center items-center w-full h-fill z-50 top-0 left-0">
+    <div className="fixed flex justify-center items-center w-full h-full z-50 top-0">
       <div className="absolute bg-ps-black opacity-70 w-full h-full"></div>
       <div className="z-10">{children}</div>
     </div>
