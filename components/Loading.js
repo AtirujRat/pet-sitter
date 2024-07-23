0da24ab026eb8a8cd8fbbac2033c407c7a0ea06a
AtirujRat <122689127+AtirujRat@users.noreply.github.com>
2024-07-23 13:22:59 +0700

    Update Loading.js

--- components/Loading.js
+++ components/Loading.js
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="h-[55vh] w-full flex justify-center">
-      <div className="loading loading-dots loading-md text-ps-orange-500"></div>
+      <div className="loading loading-dots loading-md text-ps-gray-500"></div>
     </div>
   );
 }
