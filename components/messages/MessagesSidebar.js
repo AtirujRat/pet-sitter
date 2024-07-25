24452701be98be0f45f8de4ea338b98abd374e99
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-07-25 16:48:28 +0700

    feat: add api sidebar message

--- components/messages/MessagesSidebar.js
+++ components/messages/MessagesSidebar.js
@@ -1,7 +1,49 @@
-import MessageCard from "./MeassageCard";
-import conversations from "./conversationsData";
+import { useRouter } from "next/router";
+import axios from "axios";
+import MessageCard from "./MessageCard";
+import Loading from "@/components/Loading";
+import { useEffect, useState } from "react";
+
+const API_URL = "/api/owner";
 
 export default function MessagesSidebar({ clickedCardId, onCardClick }) {
+  const router = useRouter();
+  const { id } = router.query;
+  const [conversations, setConversations] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchConversations = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        if (id) {
+          const response = await axios.get(`${API_URL}/${id}/conversations`);
+          setConversations(response.data);
+        }
+        setLoading(false);
+      } catch (error) {
+        setLoading(true);
+      }
+    };
+
+    fetchConversations();
+  }, [id]);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <div className="error-message">{error}</div>;
+  }
+
+  if (!conversations.length) {
+    return <div>No conversations found.</div>;
+  }
+
   return (
     <section>
       <div className="pt-4 w-[368px] h-full bg-ps-black">
@@ -11,10 +53,15 @@ export default function MessagesSidebar({ clickedCardId, onCardClick }) {
         {conversations.map((conversation) => (
           <MessageCard
             key={conversation.id}
-            imgUrl={conversation.imgUrl}
-            nameInterlocutor={conversation.nameInterlocutor}
-            unreadCount={conversation.unreadCount}
-            lastMessage={conversation.lastMessage}
+            imgUrl={conversation.sitters?.profile_image_url}
+            nameSitter={conversation.sitters?.full_name}
+            unreadCount={
+              conversation.messages.filter((msg) => msg.status === "unread")
+                .length
+            }
+            lastMessage={
+              conversation.messages[conversation.messages.length - 1]?.text
+            }
             isClicked={clickedCardId === conversation.id}
             onClick={() => onCardClick(conversation.id)}
           />
