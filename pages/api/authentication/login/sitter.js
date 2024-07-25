fd2250eeb7b0d10eccd3a02f0b55f217e1116535
Kim <147320315+PNKim@users.noreply.github.com>
2024-07-25 16:38:26 +0700

    Update sitter.js

--- pages/api/authentication/login/sitter.js
+++ pages/api/authentication/login/sitter.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   try {
     const { data, error } = await supabase
       .from("sitters")
-      .select("email, password")
+      .select("email, password, id")
       .eq("email", email)
       .single();
 
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
 
     return res
       .status(200)
-      .json({ message: "Sign in success", data: email_supabase, id: data.id });
+      .json({ message: "Sign in success", data: {...email_supabase, id: data.id }});
   } catch (e) {
     return res.status(400).json({ message: "error connection from database" });
   }
