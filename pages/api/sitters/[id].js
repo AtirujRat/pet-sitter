1c427f0882181f1d67c4c3766386a29cd8ce3daf
Kim <147320315+PNKim@users.noreply.github.com>
2024-07-25 15:26:36 +0700

    Update [id].js

--- pages/api/sitters/[id].js
+++ pages/api/sitters/[id].js
@@ -29,140 +29,140 @@ export default async function handler(req, res) {
     }
   }
 
-  // if (req.method === "PUT") {
-  //   const { id } = req.query;
-  //   const reqBody = { ...req.body, updated_at: new Date() };
+  if (req.method === "PUT") {
+    const { id } = req.query;
+    const reqBody = { ...req.body, updated_at: new Date() };
 
-  //   try {
-  //     // ดึงข้อมูลปัจจุบันของ sitters
-  //     const { data: existingData, error: selectError } = await supabase
-  //       .from("sitters")
-  //       .select("*")
-  //       .eq("id", id);
+    try {
+      // ดึงข้อมูลปัจจุบันของ sitters
+      const { data: existingData, error: selectError } = await supabase
+        .from("sitters")
+        .select("*")
+        .eq("id", id);
 
-  //     if (selectError) {
-  //       return res.status(405).json({ error: "Method Not Available" });
-  //     }
+      if (selectError) {
+        return res.status(405).json({ error: "Method Not Available" });
+      }
 
-  //     // เตรียมข้อมูลสำหรับการอัปเดต
-  //     const updateData = {
-  //       email: reqBody.email,
-  //       phone_number: reqBody.phone_number,
-  //       profile_image_url: reqBody.profile_image_url,
-  //       full_name: reqBody.full_name,
-  //       experience: reqBody.experience,
-  //       introduction: reqBody.introduction,
-  //       trade_name: reqBody.trade_name,
-  //       pet_types: reqBody.pet_types,
-  //       services: reqBody.services,
-  //       place_description: reqBody.place_description,
-  //       sitter_status: "waiting for approval",
-  //       updated_at: reqBody.updated_at,
-  //       last_logged_in: reqBody.last_logged_in,
-  //     };
+      // เตรียมข้อมูลสำหรับการอัปเดต
+      const updateData = {
+        email: reqBody.email,
+        phone_number: reqBody.phone_number,
+        profile_image_url: reqBody.profile_image_url,
+        full_name: reqBody.full_name,
+        experience: reqBody.experience,
+        introduction: reqBody.introduction,
+        trade_name: reqBody.trade_name,
+        pet_types: reqBody.pet_types,
+        services: reqBody.services,
+        place_description: reqBody.place_description,
+        sitter_status: "waiting for approval",
+        updated_at: reqBody.updated_at,
+        last_logged_in: reqBody.last_logged_in,
+      };
 
-  //     // อัปเดตข้อมูลในตาราง sitters
-  //     let updateResult;
-  //     if (existingData.length > 0) {
-  //       const { data, error: updateError } = await supabase
-  //         .from("sitters")
-  //         .update(updateData)
-  //         .eq("id", id)
-  //         .select();
-  //       if (updateError) {
-  //         return res.status(405).json({ error: "Method Not Available" });
-  //       }
-  //       updateResult = data;
-  //     } else {
-  //       const { data, error: upsertError } = await supabase
-  //         .from("sitters")
-  //         .upsert(updateData)
-  //         .select();
-  //       if (upsertError) {
-  //         return res.status(405).json({ error: "Method Not Available" });
-  //       }
-  //       updateResult = data;
-  //     }
+      // อัปเดตข้อมูลในตาราง sitters
+      let updateResult;
+      if (existingData.length > 0) {
+        const { data, error: updateError } = await supabase
+          .from("sitters")
+          .update(updateData)
+          .eq("id", id)
+          .select();
+        if (updateError) {
+          return res.status(405).json({ error: "Method Not Available" });
+        }
+        updateResult = data;
+      } else {
+        const { data, error: upsertError } = await supabase
+          .from("sitters")
+          .upsert(updateData)
+          .select();
+        if (upsertError) {
+          return res.status(405).json({ error: "Method Not Available" });
+        }
+        updateResult = data;
+      }
 
-  //     // ข้อมูลที่อยู่ที่ต้องการอัปเดตหรือเพิ่มใหม่
-  //     const addressData = {
-  //       address_detail: reqBody.sitters_addresses?.address_detail || "",
-  //       district: reqBody.sitters_addresses?.district || "",
-  //       sub_district: reqBody.sitters_addresses?.subDistrict || "",
-  //       province: reqBody.sitters_addresses?.province || "",
-  //       post_code: reqBody.sitters_addresses?.zip_code || "",
-  //       lat: reqBody.sitters_addresses?.lat || "",
-  //       lng: reqBody.sitters_addresses?.lng || "",
-  //       sitter_id: id,
-  //     };
+      // ข้อมูลที่อยู่ที่ต้องการอัปเดตหรือเพิ่มใหม่
+      const addressData = {
+        address_detail: reqBody.sitters_addresses?.address_detail || "",
+        district: reqBody.sitters_addresses?.district || "",
+        sub_district: reqBody.sitters_addresses?.subDistrict || "",
+        province: reqBody.sitters_addresses?.province || "",
+        post_code: reqBody.sitters_addresses?.zip_code || "",
+        lat: reqBody.sitters_addresses?.lat || "",
+        lng: reqBody.sitters_addresses?.lng || "",
+        sitter_id: id,
+      };
 
-  //     let addressResult;
+      let addressResult;
 
-  //     // ลบที่อยู่เดิมที่มี sitter_id เดียวกันออก
-  //     const { error: deleteSitterIdError } = await supabase
-  //       .from("sitters_addresses")
-  //       .delete()
-  //       .eq("sitter_id", id);
+      // ลบที่อยู่เดิมที่มี sitter_id เดียวกันออก
+      const { error: deleteSitterIdError } = await supabase
+        .from("sitters_addresses")
+        .delete()
+        .eq("sitter_id", id);
 
-  //     if (deleteSitterIdError) {
-  //       return res.status(405).json({ error: "Method Not Available" });
-  //     }
+      if (deleteSitterIdError) {
+        return res.status(405).json({ error: "Method Not Available" });
+      }
 
-  //     // แทรกที่อยู่ใหม่
-  //     const { data, error: addressInsertError } = await supabase
-  //       .from("sitters_addresses")
-  //       .insert(addressData)
-  //       .select();
-  //     if (addressInsertError) {
-  //       return res.status(405).json({ error: "Method Not Available" });
-  //     }
-  //     addressResult = data;
+      // แทรกที่อยู่ใหม่
+      const { data, error: addressInsertError } = await supabase
+        .from("sitters_addresses")
+        .insert(addressData)
+        .select();
+      if (addressInsertError) {
+        return res.status(405).json({ error: "Method Not Available" });
+      }
+      addressResult = data;
 
-  //     // อัปเดต sitter_address_id ในตาราง sitters ให้ชี้ไปยังที่อยู่ใหม่
-  //     const { data: updatedSitter, error: updateSitterError } = await supabase
-  //       .from("sitters")
-  //       .update({ sitter_address_id: addressResult[0].id })
-  //       .eq("id", id)
-  //       .select();
-  //     if (updateSitterError) {
-  //       return res.status(405).json({ error: "Method Not Available" });
-  //     }
-  //     updateResult = updatedSitter;
+      // อัปเดต sitter_address_id ในตาราง sitters ให้ชี้ไปยังที่อยู่ใหม่
+      const { data: updatedSitter, error: updateSitterError } = await supabase
+        .from("sitters")
+        .update({ sitter_address_id: addressResult[0].id })
+        .eq("id", id)
+        .select();
+      if (updateSitterError) {
+        return res.status(405).json({ error: "Method Not Available" });
+      }
+      updateResult = updatedSitter;
 
-  //     // อัปเดตข้อมูลในตาราง sitters_images
-  //     // const sittersImages = reqBody.sitters_images;
+      // อัปเดตข้อมูลในตาราง sitters_images
+      // const sittersImages = reqBody.sitters_images;
 
-  //     // ลบข้อมูล sitters_images เก่าทั้งหมดที่เชื่อมโยงกับ sitter_id นี้
-  //     const { error: deleteError } = await supabase
-  //       .from("sitters_images")
-  //       .delete()
-  //       .eq("sitter_id", id);
+      // ลบข้อมูล sitters_images เก่าทั้งหมดที่เชื่อมโยงกับ sitter_id นี้
+      const { error: deleteError } = await supabase
+        .from("sitters_images")
+        .delete()
+        .eq("sitter_id", id);
 
-  //     if (deleteError) {
-  //       return res.status(405).json({ error: "Method Not Available" });
-  //     }
+      if (deleteError) {
+        return res.status(405).json({ error: "Method Not Available" });
+      }
 
-  //     // เพิ่มข้อมูล sitters_images ใหม่
-  //     const newImages = reqBody.sitters_images.map((imgUrl) => ({
-  //       sitter_id: id,
-  //       image_url: imgUrl,
-  //     }));
+      // เพิ่มข้อมูล sitters_images ใหม่
+      const newImages = reqBody.sitters_images.map((imgUrl) => ({
+        sitter_id: id,
+        image_url: imgUrl,
+      }));
 
-  //     const { data: imageData, error: imageError } = await supabase
-  //       .from("sitters_images")
-  //       .insert(newImages);
+      const { data: imageData, error: imageError } = await supabase
+        .from("sitters_images")
+        .insert(newImages);
 
-  //     if (imageError) {
-  //       return res.status(405).json({ error: "Method Not Available" });
-  //     }
+      if (imageError) {
+        return res.status(405).json({ error: "Method Not Available" });
+      }
 
-  //     return res
-  //       .status(200)
-  //       .json({ sitters: updateResult, sitters_images: imageData });
-  //   } catch (error) {
-  //     return res.status(400).json({ error: error.message });
-  //   }
-  // } else {
-  //   return res.status(405).json({ error: "Method Not Available" });
-  // }
+      return res
+        .status(200)
+        .json({ sitters: updateResult, sitters_images: imageData });
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
+    }
+  } else {
+    return res.status(405).json({ error: "Method Not Available" });
+  }
 }
