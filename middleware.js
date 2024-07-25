64bcb7be3a25a222a3491afea2080b158a97a608
PNKim <paljapon.nait@gmail.com>
2024-07-25 11:06:05 +0700

    feat: text middleware

--- middleware.js
+++ middleware.js
@@ -1,15 +1,14 @@
 export function middleware(request) {
-  // const currentUser = request.headers?.authorization;
-  // if (currentUser && request.nextUrl.pathname.startsWith("/")) {
+  // const currentUser = request.headers.get("authorization");
+  // console.log(10);
+  // if (currentUser && !request.nextUrl.pathname.startsWith("/")) {
   //   return Response.redirect(new URL("/", request.url));
-  // } else if (
-  //   !currentUser &&
-  //   !request.nextUrl.pathname.startsWith("/login/owner")
-  // ) {
+  // }
+  // if (!currentUser && !request.nextUrl.pathname.startsWith("/")) {
   //   return Response.redirect(new URL("/login/owner", request.url));
   // }
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+  matcher: [`/sitters/:path*/booking/create`],
 };
--- pages/api/sitters.js
+++ pages/api/sitters.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   const tradeName = req.query.name;
   const petType = [req.query.pet];
   const experience = req.query.exp;
-  console.log(req.headers);
+  // console.log(req.headers);
   if (req.method === "GET") {
     try {
       let supabaseQuery = supabase
