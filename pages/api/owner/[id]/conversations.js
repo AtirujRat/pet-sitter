8c8572c8cdf78ad9e227b08d4b5842dbe2a6671d
boschaiyadej <130572668+boschaiyadej@users.noreply.github.com>
2024-07-29 10:44:11 +0700

    feat: merge chat window owner sitter

--- pages/api/owner/[id]/conversations.js
+++ pages/api/owner/[id]/conversations.js
@@ -7,7 +7,9 @@ export default async function handler(req, res) {
     try {
       const { data: conversations, error } = await supabase
         .from("conversations")
-        .select("*, messages(*), sitters(full_name, profile_image_url)),")
+        .select(
+          "*, messages(*), sitters(full_name, profile_image_url), owners(full_name, profile_image_url)"
+        )
         .eq("owner_id", id);
 
       if (error) {
@@ -16,91 +18,19 @@ export default async function handler(req, res) {
 
       if (conversations.length === 0) {
         return res.status(404).json({
-          message: `Server could not find a requested conversations to show for owner id ${id}`,
+          message: `Server could not find any requested conversations to show for owner id ${id}`,
         });
       }
 
       return res.status(200).json(conversations);
-    } catch {
+    } catch (error) {
       return res.status(500).json({
         message:
-          "Server could not read conversations because database connection",
-      });
-    }
-  } else if (req.method === "PUT") {
-    try {
-      const {
-        name,
-        type,
-        breed,
-        sex,
-        age,
-        color,
-        weight,
-        description,
-        pet_image_url,
-      } = req.body;
-
-      if (!name || !type || !breed || !sex || !age || !color || !weight) {
-        return res.status(400).json({
-          message:
-            "Server could not create pet because there are missing data from client",
-        });
-      }
-
-      const { data: updatedPet, error } = await supabase
-        .from("pets")
-        .update({
-          pet_image_url,
-          name,
-          type,
-          breed,
-          sex,
-          age,
-          color,
-          weight,
-          description,
-          updated_at: new Date(),
-        })
-        .eq("owner_id", id)
-        .eq("id", petId)
-        .single();
-
-      if (error) {
-        throw error;
-      }
-
-      return res.status(200).json({
-        message: "Updated pet successfully",
-      });
-    } catch {
-      return res.status(500).json({
-        message: "Server could not update pet because database connection",
-      });
-    }
-  } else if (req.method === "DELETE") {
-    try {
-      const { data: deletedPet, error } = await supabase
-        .from("pets")
-        .delete()
-        .eq("owner_id", id)
-        .eq("id", petId)
-        .single();
-
-      if (error) {
-        throw error;
-      }
-
-      return res.status(200).json({
-        message: "Deleted pet successfully",
-      });
-    } catch {
-      return res.status(500).json({
-        message: `Server could not delete pet id ${petId} because database connection`,
+          "Server could not read conversations because of a database connection error",
       });
     }
   } else {
-    res.setHeader("Allow", ["GET", "PUT", "DELETE"]);
+    res.setHeader("Allow", ["GET"]);
     return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
