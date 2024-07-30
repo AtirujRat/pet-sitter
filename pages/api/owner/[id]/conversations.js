bed1b63068060c1fd0e3e310562cf1c7ff7fe0d4
PNKim <paljapon.nait@gmail.com>
2024-07-30 13:59:03 +0700

    fix: repeat create converstion

--- pages/api/owner/[id]/conversations.js
+++ pages/api/owner/[id]/conversations.js
@@ -34,17 +34,41 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     const { sitter_id, owner_id } = req.body;
     try {
-      const { data, error } = await supabase
+      let { data: conversations, error: errors } = await supabase
         .from("conversations")
-        .insert([{ sitter_id: sitter_id, owner_id: owner_id }])
-        .select();
+        .select("*")
+        .eq("owner_id", owner_id)
+        .eq("sitter_id", sitter_id);
 
-      if (error) {
+      if (!conversations[0]) {
+        const { data, error } = await supabase
+          .from("conversations")
+          .insert([{ sitter_id: sitter_id, owner_id: owner_id }])
+          .select();
+
+        if (errors) {
+          return res
+            .status(400)
+            .json({ message: "error connection from database" });
+        }
+        return res.status(200).json({ message: "create conversation success" });
+      } else {
+        const { data, error } = await supabase
+          .from("conversations")
+          .update({ updated_at: new Date() })
+          .eq("owner_id", owner_id)
+          .eq("sitter_id", sitter_id)
+          .select();
+
+        if (error) {
+          return res
+            .status(400)
+            .json({ message: "error connection from database" });
+        }
         return res
-          .status(400)
-          .json({ message: "error connection from database" });
+          .status(201)
+          .json({ message: "already create conversation success" });
       }
-      return res.status(200).json({ message: "create conversation success" });
     } catch (e) {
       return res
         .status(400)
