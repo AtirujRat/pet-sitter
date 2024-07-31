5ee88cd6383ea2d29613349c3d6be23c597c07d3
PNKim <paljapon.nait@gmail.com>
2024-07-31 16:58:51 +0700

    fix: create space

--- pages/api/owner/[id]/createbooking.js
+++ pages/api/owner/[id]/createbooking.js
@@ -44,6 +44,7 @@ export default async function handler(req, res) {
           },
         ])
         .select();
+
       if (error) {
         return res.status(400).json({ message: "Error from insert data" });
       }
