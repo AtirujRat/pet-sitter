1f43e91343f62ca05531df0e65040f8b10be3d83
wiKanok <kanokwan1401.ks@gmail.com>
2024-07-31 14:13:21 +0700

    feat:add admin petsitter

---
+++ components/admin/PetSitter.js
@@ -0,0 +1,130 @@
+import Image from "next/image";
+import { useAdminPetSitter } from "@/context/AdminPetSitter";
+import { useSitters } from "@/context/SittersProvider";
+
+import searchIcon from "@/public/assets/icons/icon-search.svg";
+import userImage from "@/public/assets/account/profile_white.svg";
+import PetSitterDetail from "./petsitter/PetSitterDetail";
+
+export default function PetSitter() {
+  const { setLoading, refresh, setRefresh } = useSitters();
+  const {
+    sitters,
+    search,
+    setSearch,
+    handleSitterClick,
+    selectedStatus,
+    setSelectedStatus,
+    selectedSitter,
+    setSelectedSitter,
+    getStatusComponent,
+  } = useAdminPetSitter();
+
+  return (
+    <div className="w-full p-10 flex flex-col gap-6">
+      {/* Search */}
+      {!selectedSitter ? (
+        <>
+          <div className="w-full flex justify-between items-center">
+            <p className="text-h3 pb-2 ">Pet Sitter</p>
+            <div className="flex gap-6 ">
+              <div className="relative">
+                <input
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                  type="text"
+                  placeholder="Search..."
+                  className="w-full sm:w-[240px] h-[48px] outline-none ring-0 border-[#DCDFED] text-[#7B7E8F] font-normal text-[16px] rounded-lg"
+                />
+                <Image
+                  src={searchIcon}
+                  alt="search"
+                  className="absolute right-0 bottom-0 -translate-y-3 -translate-x-4"
+                />
+              </div>
+              <select
+                value={selectedStatus}
+                onChange={(e) => {
+                  setSelectedStatus(e.target.value);
+                  setRefresh(!refresh);
+                }}
+                className="w-full sm:w-[240px] h-[48px] outline-none ring-0 border-[#DCDFED] text-[#7B7E8F] font-normal text-[16px] rounded-lg"
+              >
+                <option selected value="">
+                  All status
+                </option>
+                <option value="waiting for approve">Waiting for approve</option>
+                <option value="approved">Approved</option>
+                <option value="rejected">Rejected</option>
+              </select>
+            </div>
+          </div>
+
+          {/* Pet Sitter Table */}
+
+          <div className="bg-ps-white rounded-2xl overflow-x-auto">
+            <table className="table table-fixed">
+              {/* head */}
+              <thead className="h-[48px] bg-ps-black">
+                <tr className="w-full">
+                  <th className="w-[20%] text-b3 text-ps-white shrink-0">
+                    Full Name
+                  </th>
+                  <th className="w-[20%] text-b3 text-ps-white shrink-0">
+                    Pet Sitter Name
+                  </th>
+                  <th className="w-[40%] text-b3 text-ps-white shrink-0">
+                    Email
+                  </th>
+                  <th className="text-b3 text-ps-white shrink-0">Status</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {sitters.map((sitter, index) => (
+                  <tr
+                    key={index}
+                    className="hover:bg-ps-orange-100 cursor-pointer"
+                    onClick={() => handleSitterClick(sitter)}
+                  >
+                    <td className="text-b2 py-6 w-full flex items-center gap-[10px]">
+                      <div className="relative w-[44px] h-[44px] overflow-hidden rounded-full bg-ps-gray-200">
+                        {sitter?.profile_image_url ? (
+                          <Image
+                            src={sitter?.profile_image_url || null}
+                            alt={sitter?.full_name}
+                            fill
+                            style={{ objectFit: "cover" }}
+                          />
+                        ) : (
+                          <Image
+                            src={userImage}
+                            alt="userImage"
+                            width={20}
+                            height={20}
+                            style={{ objectFit: "cover" }}
+                          />
+                        )}
+                      </div>
+                      {sitter?.full_name}
+                    </td>
+                    <td className="text-b2 py-6">{sitter?.trade_name}</td>
+                    <td className="text-b2 py-6">{sitter?.email}</td>
+                    <td className="text-b2 py-6">
+                      {getStatusComponent(sitter?.sitter_status)}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </>
+      ) : (
+        <PetSitterDetail
+          sitter={selectedSitter}
+          closeDetail={() => setSelectedSitter(null)}
+        />
+      )}
+    </div>
+  );
+}
---
+++ context/AdminPetSitter.js
@@ -0,0 +1,99 @@
+import axios from "axios";
+import { createContext, useState, useContext, useEffect } from "react";
+import { useSearch } from "@/context/Search";
+
+import {
+  Approved,
+  WaitingForApproval,
+  Rejected,
+} from "@/components/sitters/profile/SittersStatus";
+
+const AdminPetSitterContext = createContext();
+export function useAdminPetSitter() {
+  return useContext(AdminPetSitterContext);
+}
+
+export function AdminPetSitterProvider(props) {
+  const { location } = useSearch();
+
+  const [sitters, setSitters] = useState([]);
+  const [search, setSearch] = useState("");
+  const [selectedStatus, setSelectedStatus] = useState("");
+  const [getCurrentSitter, setGetCurrentSitter] = useState([]);
+  const [selectedSitter, setSelectedSitter] = useState(null);
+
+  async function GetSitterProfile() {
+    try {
+      const response = await axios.get(
+        `/api/sitters/getsitters?name=${search}&tradeName=${search}&email=${search}&status=${selectedStatus}`
+      );
+      setSitters(response.data.data);
+      if (response.data.data.sitters_addresses) {
+        location({
+          lat: Number(response.data.data.sitters_addresses.lat),
+          lng: Number(response.data.data.sitters_addresses.lng),
+        });
+      } else {
+        console.warn("No address data found for this sitter.");
+      }
+    } catch (error) {
+      console.error("Error fetching profile data:", error);
+    }
+  }
+
+  function handleSitterClick(sitter) {
+    setSelectedSitter(sitter);
+    setGetCurrentSitter(sitter);
+  }
+
+  const sitterStatus = {
+    approved: Approved,
+    waitingforapproval: WaitingForApproval,
+    rejected: Rejected,
+  };
+
+  const getStatusComponent = (status) => {
+    const statusKey = status?.replace(/\s+/g, "").toLowerCase();
+    const StatusComponent = sitterStatus[statusKey];
+    return StatusComponent ? <StatusComponent /> : status;
+  };
+
+  const debounce = (func, delay = 500) => {
+    let timer;
+    return function (...args) {
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        func(...args);
+      }, delay);
+    };
+  };
+
+  useEffect(() => {
+    const debouncedGetSitter = debounce(GetSitterProfile, 500);
+    debouncedGetSitter();
+  }, [search]);
+
+  useEffect(() => {
+    GetSitterProfile();
+  }, []);
+
+  return (
+    <AdminPetSitterContext.Provider
+      value={{
+        sitters,
+        search,
+        setSearch,
+        selectedStatus,
+        setSelectedStatus,
+        getCurrentSitter,
+        setGetCurrentSitter,
+        handleSitterClick,
+        selectedSitter,
+        setSelectedSitter,
+        getStatusComponent,
+      }}
+    >
+      {props.children}
+    </AdminPetSitterContext.Provider>
+  );
+}
