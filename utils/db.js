edd39cd2855ee5af00441ed0bc6b68d5980ab1ef
Kim <147320315+PNKim@users.noreply.github.com>
2024-08-05 11:24:53 +0700

    Delete utils/db.js

--- utils/db.js
+++
@@ -1,8 +0,0 @@
-import * as pg from "pg";
-import "dotenv/config";
-const { Pool } = pg.default;
-const connectionPool = new Pool({
-  connectionString:
-    "postgresql://postgres.etraoduqrzijngbazoib:awyLzNpZiuIvJfhs@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres",
-});
-export default connectionPool;
