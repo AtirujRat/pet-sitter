879098fea2ef62edb5e4673702da725feea4588a
PNKim <paljapon.nait@gmail.com>
2024-08-05 13:57:04 +0700

    feat: add model alert

--- components/booking/create/Payment.js
+++ components/booking/create/Payment.js
@@ -1,7 +1,6 @@
 import CreditCardPayment from "./CreditCardPayment";
 import CashPayment from "./CashPayment";
-import { useContext } from "react";
-import { BookingContext, useBooking } from "@/context/Booking";
+import { useBooking } from "@/context/Booking";
 export default function Payment() {
   const { payment, setPayment } = useBooking();
   return (
