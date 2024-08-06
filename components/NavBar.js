b4cc4425ec24aaeb3ca40dc3d230a2973e2150ba
PNKim <paljapon.nait@gmail.com>
2024-08-06 13:48:10 +0700

    feat: add pet-sitter navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,23 +3,18 @@ import Image from "next/image";
 import { supabase } from "@/utils/supabase";
 import bell from "@/public/assets/navbar/bell.svg";
 import message from "@/public/assets/navbar/message.svg";
-import usermock from "@/public/assets/navbar/usermock.svg";
-import profile from "@/public/assets/navbar/profile.svg";
-import pet from "@/public/assets/navbar/pet.svg";
-import history from "@/public/assets/navbar/history.svg";
-import logout from "@/public/assets/navbar/logout.svg";
 import menu from "@/public/assets/navbar/menu.svg";
 import sisterlogo from "@/public/assets/sister-logo.svg";
 import Link from "next/link";
 import axios from "axios";
 import { useUser } from "@/context/User";
-import { useRouter } from "next/router";
+import Anonymous from "@/components/navbar/Anonymous";
+import OwnerUser from "@/components/navbar/OwnerUser";
+import SitterUser from "@/components/navbar/SitterUser";
 
 export default function NavBar({ setOpenModal }) {
   const [userData, setUserData] = useState();
-  const [userId, setUserId] = useState();
   const { userInfo, setUserInfo } = useUser();
-  const router = useRouter();
 
   async function getUser() {
     const {
@@ -39,58 +34,31 @@ export default function NavBar({ setOpenModal }) {
       }
     }
     if (error) {
-      console.error("error");
       return;
     }
     setUserData(user);
 
-    const { data: owners_id, error: getIdError } = await supabase
-      .from("owners")
-      .select("id")
-      .eq("email", user.email);
-
-    if (getIdError) {
-      console.log(getIdError);
-    }
-    if (user) {
-      if (
-        user.app_metadata.provider !== "email" ||
-        (user.app_metadata.providers.includes("google") && !userInfo.role)
-      ) {
-        setTimeout(() => {
-          setUserInfo({ role: "owner", id: owners_id[0].id });
-        }, 500);
-      }
-      setUserId(owners_id[0].id);
-    }
-
     if (!userInfo.role) {
       setUserData();
-      setUserId();
     }
   }
 
   const newUser = async (data) => {
     try {
       await axios.post("/api/authentication/register/owner", data);
-      console.log("success");
+      const id = await axios.post("/api/owner/getowners", {
+        email: data.email,
+      });
+      setTimeout(() => {
+        setUserInfo({ role: "owner", id: id.data.data });
+      }, 500);
     } catch (e) {
-      console.log(e);
+      return;
     }
   };
   useEffect(() => {
     getUser();
   }, [userInfo]);
-
-  const handleLogout = async () => {
-    let { error } = await supabase.auth.signOut();
-    if (error) return;
-    setUserData(undefined);
-    setTimeout(() => {
-      localStorage.removeItem("userInfo");
-      router.reload();
-    }, 500);
-  };
 
   return (
     <nav className="w-full flex justify-center py-5 px-5 lg:px-0 relative z-10">
@@ -102,91 +70,18 @@ export default function NavBar({ setOpenModal }) {
         <div className="flex md:gap-4 gap-2 items-center">
           {userData !== undefined ? (
             userInfo?.role === "owner" ? (
-              <>
-                <Image
-                  src={bell}
-                  alt="bell"
-                  width={48}
-                  className="w-[48px] h-[48px] bg-[#F6F6F9] rounded-full cursor-pointer "
-                />
-                <Link href={`/owners/${userId}/messages/`}>
-                  <Image
-                    src={message}
-                    alt="message"
-                    width={48}
-                    className="w-[48px] h-[48px] bg-[#F6F6F9] rounded-full cursor-pointer mr-1"
-                  />
-                </Link>
-
-                <div className="flex rounded-full w-[48px] h-[48px] dropdown dropdown-bottom">
-                  <Image
-                    src={usermock}
-                    alt="bell"
-                    className=" cursor-pointer"
-                    tabIndex="0"
-                    role="button"
-                    width={48}
-                  />
-                  <div>
-                    <ul
-                      tabIndex="0"
-                      className="dropdown-content menu bg-base-100 rounded-lg z-[1] w-52 p-2 text-b2 drop-shadow-costom px-0"
-                    >
-                      <li className="py-2 text-b2">
-                        <Link href={`/owners/${userId}/profile/`}>
-                          <Image src={profile} alt="profile" width={20} />
-                          Profile
-                        </Link>
-                      </li>
-                      <li className="py-2 text-b2">
-                        <Link href={`/owners/${userId}/yourpet/`}>
-                          <Image src={pet} alt="your pet" width={22} />
-                          Your Pet
-                        </Link>
-                      </li>
-                      <li className="py-2 text-b2">
-                        <Link href={`/owners/${userId}/bookinghistory/`}>
-                          <Image src={history} alt="history" width={20} />
-                          History
-                        </Link>
-                      </li>
-                      <div className="border-b border-[#DCDFED]"></div>
-                      <li
-                        className="py-2 cursor-pointer text-b2"
-                        onClick={handleLogout}
-                      >
-                        <button>
-                          <Image src={logout} alt="logout" width={14} />
-                          Log out
-                        </button>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </>
+              <OwnerUser
+                userId={userInfo.id}
+                userEmail={userData.email}
+                setUserData={setUserData}
+              />
             ) : (
-              <>
-                <Link href={"/register/sitter"}>
-                  <button className="py-4 px-6 text-b1">
-                    Become a Pet Sitter
-                  </button>
-                </Link>
-                <Link href={"/login/owner"}>
-                  <button className="py-4 px-6 text-b1 ">Login</button>
-                </Link>
-              </>
+              userInfo?.role === "sitter" && (
+                <SitterUser userId={userInfo.id} setUserData={setUserData} />
+              )
             )
           ) : (
-            <>
-              <Link href={"/register/sitter"}>
-                <button className="py-4 px-6 text-b1">
-                  Become a Pet Sitter
-                </button>
-              </Link>
-              <Link href={"/login/owner"}>
-                <button className="py-4 px-6 text-b1 ">Login</button>
-              </Link>
-            </>
+            <Anonymous />
           )}
           <Link href={"/sitters"}>
             <button className="items-center justify-center w-[168px] h-[48px] bg-ps-orange-500 text-ps-white text-[16px] font-bold rounded-full tracking-wide ">
