358ed44ba0c7d74ddcaf776ea56eb009df2ca9f4
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-08-06 15:04:44 +0700

    refactor: get id from user info

--- pages/owners/yourpet/index.js
+++ pages/owners/yourpet/index.js
@@ -7,9 +7,7 @@ import { useOwnersAccountState } from "@/context/OwnersAccountState";
 
 export default function PetListPage() {
   const router = useRouter();
-  const { id } = router.query;
 
-  const [isModalOpen, setIsModalOpen] = useState(true);
   const { accountState, changeAccountStateHandle } = useOwnersAccountState();
 
   useEffect(() => {
@@ -17,16 +15,16 @@ export default function PetListPage() {
   }, []);
 
   return (
-    <section className="w-full h-full bg-ps-gray-100 max-md:pt-0 pt-10 pb-20 max-md:pb-0">
-      <div className="max-w-[1440px] min-w-0 sm:flex sm:justify-between mx-auto max-md:flex-col sm:items-start lg:px-20  gap-9">
+    <section className="w-full h-full bg-ps-gray-100 max-md:pt-0 lg:pt-10 pb-20 max-md:pb-0">
+      <div className="max-w-[1440px] min-w-0 lg:flex lg:justify-between mx-auto max-lg:flex-col lg:items-start lg:px-20  gap-9">
         <SideBarOwners />
-        <div className="bg-ps-white max-sm:bg-ps-gray-100 rounded-2xl max-sm:rounded-none w-[956px] min-h-[824px] max-md:w-[95%] max-md:mx-auto max-sm:w-full max-md:min-h-[678px] max-sm:shadow-none p-10 max-sm:px-2 flex flex-col shadow-md gap-12">
+        <div className="bg-ps-white max-sm:bg-ps-gray-100 rounded-2xl max-lg:rounded-none w-[956px] min-h-[824px] max-lg:w-full max-md:mx-auto max-sm:w-full max-md:min-h-[678px] max-lg:shadow-none p-10 max-sm:px-2 flex flex-col shadow-md gap-12">
           <div className="flex justify-between items-center">
             <p className="text-h3">Your Pet</p>
             <ButtonOrange
               text="Create Pet"
               width="w-fit"
-              onClick={() => router.push(`/owners/${id}/yourpet/create`)}
+              onClick={() => router.push(`/owners/yourpet/create`)}
             />
           </div>
           <PetList />
