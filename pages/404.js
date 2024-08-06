e0ad3bed267f33a43a7da4327c68f26d9428a0ce
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-08-06 09:19:18 +0700

    fix: word 404

--- pages/404.js
+++ pages/404.js
@@ -1,24 +1,29 @@
 import { useRouter } from "next/router";
 import Image from "next/image";
 import { ButtonOrange } from "@/components/buttons/OrangeButtons";
-import BackgroudAuth from "@/components/authentication/BackgroundAuth";
+import BackgroundAuth from "@/components/authentication/BackgroundAuth";
 
 export default function Custom404() {
   const router = useRouter();
   return (
     <div className="h-full w-full flex justify-center">
       <div className="max-lg:hidden">
-        <BackgroudAuth />
+        <BackgroundAuth />
       </div>
       <div className="flex flex-col justify-center items-center mt-32 max-sm:mt-48 gap-5 px-10">
-        <Image src="/assets/404/image-404.svg" width={600} height={500} />
+        <Image
+          src="/assets/404/image-404.svg"
+          alt="image-404"
+          width={600}
+          height={500}
+        />
         <div className="flex flex-col justify-center items-center gap-5">
           <div className="flex flex-col gap-1 w-full">
             <h2 className="text-h2 max-sm:text-h3 text-center">
               Something went wrong.
             </h2>
             <h3 className="text-b1max-sm:text-b2 text-center">
-              Sorry, We can't find the page you're looking for.
+              Sorry, We can not find the page you are looking for.
             </h3>
           </div>
           <ButtonOrange
