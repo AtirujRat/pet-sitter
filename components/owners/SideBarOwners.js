b0a516d384cfa6d58099eb843fa92a9a28462203
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-08-08 12:02:44 +0700

    feat: remove update password

--- components/owners/SideBarOwners.js
+++ components/owners/SideBarOwners.js
@@ -15,9 +15,9 @@ function SideBarOwners() {
 
   return (
     <section className="w-full lg:w-fit h-fit md:sticky top-5">
-      <div className="flex flex-col items-start w-full lg:w-[292px] lg:h-[356px] lg:rounded-2xl bg-ps-white lg:shadow-md overflow-x-scroll lg:overflow-hidden z-50">
+      <div className="flex flex-col items-start w-full lg:w-[292px] lg:h-fit lg:rounded-2xl bg-ps-white lg:shadow-md max-sm:overflow-x-scroll lg:overflow-hidden z-50">
         <h1 className="text-h4 hidden lg:block px-[20px] py-[20px]">Account</h1>
-        <div className="text-b1 flex justify-between min-w-max md:min-w-full lg:flex-col">
+        <div className="text-b1 flex justify-between min-w-max lg:min-w-full lg:flex-col">
           <button
             onClick={() => {
               changeAccountStateHandle("profile");
@@ -77,23 +77,6 @@ function SideBarOwners() {
               alt="booking history icon"
             />
             Booking History
-          </button>
-          <button
-            onClick={() => {}}
-            className={`flex gap-5 ${
-              accountState === "changepassword"
-                ? "bg-ps-orange-100 text-ps-orange-500"
-                : "text-ps-gray-500"
-            }  py-[20px] px-[20px] hover:text-ps-orange-500 `}
-          >
-            <Image
-              className="w-[25px] h-[24px] hover:text-ps-orange-500"
-              src={
-                accountState === "changepassword" ? list_icon_active : list_icon
-              }
-              alt="change password icon"
-            />
-            Change Password
           </button>
         </div>
       </div>
