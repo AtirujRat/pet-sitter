c4b16a8ff56f79d5f94f645001709f70f4077c65
wiKanok <kanokwan1401.ks@gmail.com>
2024-08-08 10:22:58 +0700

    fix: loading sitter manage profile

--- pages/sitters/booking/[bookingId]/index.js
+++ pages/sitters/booking/[bookingId]/index.js
@@ -1,7 +1,6 @@
-import axios from "axios";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { useUser } from "@/context/User";
+import { useSitterManageProfileState } from "@/context/SitterManageProfile";
+
 import NavBarSitter from "@/components/sitters/NavbarSitter";
 import SideBarSitter from "@/components/sitters/SideBarSitter";
 import Loading from "@/components/Loading";
@@ -10,58 +9,30 @@ import SidebarSitterMobile from "@/components/sitters/mobile/SidebarSitterMobile
 
 export default function Detail() {
   const router = useRouter();
+  const { profile } = useSitterManageProfileState();
   const { bookingId } = router.query;
-  const { userInfo, connection } = useUser();
-  const id = userInfo?.id;
-
-  const [profile, setProfile] = useState(null);
-
-  const GetProfile = async () => {
-    try {
-      if (id) {
-        const response = await axios.get(`/api/sitters/${id}`);
-        setProfile(response.data.data[0]);
-      }
-    } catch (error) {
-      console.error("Error fetching profile data:", error);
-    }
-  };
-
-  useEffect(() => {
-    GetProfile();
-  }, [id]);
-
-  if (!profile) {
-    return (
-      <div>
-        <Loading />
-      </div>
-    );
-  }
 
   return (
-    <>
-      {userInfo?.role === "sitter" ? (
-        <div className="flex">
-          <SideBarSitter />
-          <div className="w-full flex-col">
-            <NavBarSitter
-              profileImage={profile?.profile_image_url}
-              fullName={profile?.full_name}
-            />
-            <div className="w-full">
-              <SidebarSitterMobile />
-            </div>
-            <div className="bg-ps-gray-100 flex flex-col gap-6 md:p-10 p-4">
-              <div>
-                <BookingListDetail bookingId={bookingId} />
-              </div>
+    <div className="flex">
+      <SideBarSitter />
+      <div className="w-full flex-col">
+        <NavBarSitter
+          profileImage={profile?.profile_image_url}
+          fullName={profile?.full_name}
+        />
+        <div className="w-full">
+          <SidebarSitterMobile />
+        </div>
+        {!profile ? (
+          <Loading />
+        ) : (
+          <div className="bg-ps-gray-100 flex flex-col gap-6 md:p-10 p-4">
+            <div>
+              <BookingListDetail bookingId={bookingId} />
             </div>
           </div>
-        </div>
-      ) : (
-        <Loading />
-      )}
-    </>
+        )}
+      </div>
+    </div>
   );
 }
