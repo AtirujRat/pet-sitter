4d9b61465091162fbc8e825a8a818d203d62ac94
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-08-09 14:41:37 +0700

    fix: responsive review modal

--- components/review/YourReview.js
+++ components/review/YourReview.js
@@ -14,7 +14,11 @@ export default function YourReview(props) {
   return (
     <div className="max-[768px]:absolute left-0 bottom-0 w-[100vw] h-[70vh] md:w-[800px] md:h-[720px] bg-ps-white rounded-t-2xl  md:rounded-2xl">
       <div className="flex max-w-full justify-between border-b-[1px] border-ps-gray-200 py-[24px] px-[40px]">
-        <h1 className="text-h3 text-ps-gray-600">Your Rating and Review</h1>
+        <h1 className="text-h3 text-ps-gray-600">
+          <span className="max-md:hidden">Your </span>Rating{" "}
+          <span className="max-md:hidden">and</span>
+          <span className="md:hidden">&</span> Review
+        </h1>
         <Image
           onClick={() => props.closeModal()}
           className="cursor-pointer"
@@ -22,36 +26,45 @@ export default function YourReview(props) {
           alt="cross icon"
         />
       </div>
-      <div className="flex items-center justify-between flex-col gap-[350px] p-[40px]">
-        <div className="w-full  flex  justify-start gap-[16px] pb-[40px] pt-[24px] px-[24px] border-b-[1px] border-ps-gray-200">
-          <div className="w-[220px] flex items-center gap-[16px] ">
-            <img
-              className="w-[56px] h-[56px] rounded-full object-cover"
-              src={props.ownerData[0].profile_image_url}
-              alt="cross icon"
-            />
-            <div>
-              <h1 className="text-b1">{props.ownerData[0].full_name}</h1>
-              <h1 className="text-ps-gray-400 text-b3">
-                <GetOnlyDate time={props.ownerData[0].updated_at} />
-              </h1>
+      <div className="flex items-center md:h-[640px] h-[560px] justify-between flex-col sm:p-10 p-5">
+        <div className="w-full flex flex-col gap-3 border-b-2 pb-10 border-b-ps-gray-200">
+          <div className="flex justify-between">
+            <div className="flex items-start gap-3 sm:pr-10">
+              <img
+                className="sm:w-[56px] sm:h-[56px] w-[36px] h-[36px] rounded-full object-cover"
+                src={props.ownerData[0].profile_image_url}
+                alt="cross icon"
+              />
+              <div>
+                <h1 className="text-b1">{props.ownerData[0].full_name}</h1>
+                <h1 className="text-ps-gray-400 text-b3">
+                  <GetOnlyDate time={props.ownerData[0].updated_at} />
+                </h1>
+              </div>
+            </div>
+            <div className="flex flex-col gap-3">
+              <div className="flex items-center gap-2">
+                {[0, 1, 2, 3, 4].map((item, index) => {
+                  return (
+                    <Image
+                      key={item}
+                      className="w-[20px] h-[20px]"
+                      src={
+                        index + 1 <= reviews[0].rating ? green_star : gray_star
+                      }
+                      alt="stars icon"
+                    />
+                  );
+                })}
+              </div>
+              <div className="max-md:hidden">
+                <h1 className="text-ps-gray-500 text-b2">
+                  {reviews[0].description}
+                </h1>
+              </div>
             </div>
           </div>
-          <div className="flex items-center flex-col justify-between">
-            <div className="flex items-center gap-[2px] h-[64px]">
-              {[0, 1, 2, 3, 4].map((item, index) => {
-                return (
-                  <Image
-                    key={item}
-                    className="w-[20px] h-[20px]"
-                    src={
-                      index + 1 <= reviews[0].rating ? green_star : gray_star
-                    }
-                    alt="stars icon"
-                  />
-                );
-              })}
-            </div>
+          <div className="mx-2 md:hidden">
             <h1 className="text-ps-gray-500 text-b2">
               {reviews[0].description}
             </h1>
