27eb6a0a6b55c7a034fe0b02aba43a97bb6922ce
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-08-09 17:20:43 +0700

    feat: setup google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,58 @@
 import "@/styles/globals.css";
 import Layout from "@/components/layout";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Script from "next/script";
+
+const GA_TRACKING_ID = "G-NEV08CKQK8"; // Your Google Analytics ID
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag("config", GA_TRACKING_ID, {
+        page_path: url,
+      });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   const getLayout = Component.getLayout;
-  if (getLayout) {
-    return getLayout(<Component {...pageProps} />);
-  } else
-    return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    );
+
+  return (
+    <>
+      {/* Google Analytics script */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
+      {/* Render the layout */}
+      {getLayout ? (
+        getLayout(<Component {...pageProps} />)
+      ) : (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
+    </>
+  );
 }
